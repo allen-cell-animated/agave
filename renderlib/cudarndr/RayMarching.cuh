64f2f92d759433c4f355d8e0db09d0052545265f
dmt <danielt@alleninstitute.org>
2018-02-01 09:33:36 -0800

    small optimization: unneeded shared memory, also fixes race condition warning

--- renderlib/cudarndr/RayMarching.cuh
+++ renderlib/cudarndr/RayMarching.cuh
@@ -9,16 +9,14 @@
 
 DEV inline bool SampleDistanceRM(CRay& R, CRNG& RNG, Vec3f& Ps, const cudaVolume& volumedata)
 {
-	const int TID = threadIdx.y * blockDim.x + threadIdx.x;
+	float MinT;
+	float MaxT;
 
-	__shared__ float MinT[KRNL_SS_BLOCK_SIZE];
-	__shared__ float MaxT[KRNL_SS_BLOCK_SIZE];
-
-	if (!IntersectBox(R, &MinT[TID], &MaxT[TID]))
+	if (!IntersectBox(R, &MinT, &MaxT))
 		return false;
 
-	MinT[TID] = max(MinT[TID], R.m_MinT);
-	MaxT[TID] = min(MaxT[TID], R.m_MaxT);
+	MinT = max(MinT, R.m_MinT);
+	MaxT = min(MaxT, R.m_MaxT);
 
 	// ray march along the ray's projected path and keep an average sigmaT value.
 	// When the distance has become greater than the average sigmaT value given by -log(RandomFloat[0, 1]) / averageSigmaT 
@@ -28,19 +26,19 @@ DEV inline bool SampleDistanceRM(CRay& R, CRNG& RNG, Vec3f& Ps, const cudaVolume
 	float Sum		= 0.0f;
 	float SigmaT	= 0.0f;
 
-	MinT[TID] += RNG.Get1() * gStepSize;
+	MinT += RNG.Get1() * gStepSize;
 	int ch = 0;
 	while (Sum < S)
 	{
-		Ps = R.m_O + MinT[TID] * R.m_D;
+		Ps = R.m_O + MinT * R.m_D;
 
-		if (MinT[TID] > MaxT[TID])
+		if (MinT > MaxT)
 			return false;
 		
 		SigmaT = gDensityScale * GetOpacity(GetNormalizedIntensityMax4ch(Ps, volumedata, ch));
 
 		Sum			+= SigmaT * gStepSize;
-		MinT[TID]	+= gStepSize;
+		MinT	+= gStepSize;
 	}
 
 	return true;
@@ -48,35 +46,33 @@ DEV inline bool SampleDistanceRM(CRay& R, CRNG& RNG, Vec3f& Ps, const cudaVolume
 
 DEV inline bool FreePathRM(CRay& R, CRNG& RNG, const cudaVolume& volumedata)
 {
-	const int TID = threadIdx.y * blockDim.x + threadIdx.x;
+	float MinT;
+	float MaxT;
+	Vec3f Ps;
 
-	__shared__ float MinT[KRNL_SS_BLOCK_SIZE];
-	__shared__ float MaxT[KRNL_SS_BLOCK_SIZE];
-	__shared__ Vec3f Ps[KRNL_SS_BLOCK_SIZE];
-
-	if (!IntersectBox(R, &MinT[TID], &MaxT[TID]))
+	if (!IntersectBox(R, &MinT, &MaxT))
 		return false;
 
-	MinT[TID] = max(MinT[TID], R.m_MinT);
-	MaxT[TID] = min(MaxT[TID], R.m_MaxT);
+	MinT = max(MinT, R.m_MinT);
+	MaxT = min(MaxT, R.m_MaxT);
 
 	const float S	= -log(RNG.Get1()) / gDensityScale;
 	float Sum		= 0.0f;
 	float SigmaT	= 0.0f;
 
-	MinT[TID] += RNG.Get1() * gStepSizeShadow;
+	MinT += RNG.Get1() * gStepSizeShadow;
 	int ch = 0;
 	while (Sum < S)
 	{
-		Ps[TID] = R.m_O + MinT[TID] * R.m_D;
+		Ps = R.m_O + MinT * R.m_D;
 
-		if (MinT[TID] > MaxT[TID])
+		if (MinT > MaxT)
 			return false;
 		
-		SigmaT = gDensityScale * GetOpacity(GetNormalizedIntensityMax4ch(Ps[TID], volumedata, ch));
+		SigmaT = gDensityScale * GetOpacity(GetNormalizedIntensityMax4ch(Ps, volumedata, ch));
 
 		Sum			+= SigmaT * gStepSizeShadow;
-		MinT[TID]	+= gStepSizeShadow;
+		MinT	+= gStepSizeShadow;
 	}
 
 	return true;
