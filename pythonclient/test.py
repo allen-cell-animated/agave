7b5cf79272f2e8fe10662d6cfb19f79ab866161e
dmt <danielt@alleninstitute.org>
2018-02-23 09:59:22 -0800

    various improvements to scripted python rendering client

--- pythonclient/test.py
+++ pythonclient/test.py
@@ -4,29 +4,30 @@ import io
 import math
 from PIL import Image
 from commandbuffer import CommandBuffer
+from collections import deque
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+# import vtk
 
 import numpy
 from aicsimage.io.tifReader import TifReader
 from aicsimage.io.omeTifWriter import OmeTifWriter
-from aicsimage.io.omexml import OMEXML
-from aicsimage.io.omexml import qn
 
-import numpy
-from aicsimage.io.tifReader import TifReader
-from aicsimage.io.omeTifWriter import OmeTifWriter
-
-OUTROOT = '\\\\allen\\aics\\animated-cell\\Dan\\2018-02-14_dan_vday_mitosis\\timelapse_wt2_s2\\'
+OUTROOT = '//allen/aics/animated-cell/Dan/2018-02-14_dan_vday_mitosis/timelapse_wt2_s2/'
 CHNAMES = [
     'dna',
     'fibrillarin',
     'lamin_b1',
-    'tom20'
+    'tom20',
+    'brightfield'
 ]
 INFILES = [
     'prediction_dna.tiff',
     'prediction_fibrillarin.tiff',
     'prediction_lamin_b1.tiff',
-    'prediction_tom20.tiff'
+    'prediction_tom20.tiff',
+    'signal.tiff'
 ]
 
 
@@ -79,33 +80,61 @@ def convertFiles():
             outfilepath = OUTROOT + CHNAMES[i] + '_frame_' + str(j).zfill(2) + '.ome.tiff'
             convert_tiff_to_ome_tiff_1ch(infilepath, outfilepath)
 
-N = 0
+# assumptions: every commandbuffer send should result in one image.
+# also, they arrive in the order the buffers were sent.
 class DummyClient(WebSocketClient):
-    def opened(self):
-        print("opened up")
+    def push_request(self, cb, id):
+        buf = cb.make_buffer()
+        self.send(buf, True)
+        self.queue.append(id)
 
-        # 00 .. 19
-        # for j in range(0, len(CHNAMES)):
-        #     for i in range(0, 20):
-        #         outfilepath = OUTROOT + CHNAMES[j] + '_frame_' + str(i).zfill(2) + '.ome.tiff'
-        #
-        #         cb = CommandBuffer()
-        #         cb.add_command("LOAD_OME_TIF", outfilepath)
-        #
-        #         # render
-        #         cb.add_command("SET_RESOLUTION", 1024, 1024)
-        #         cb.add_command("RENDER_ITERATIONS", 64)
-        #         cb.add_command("FRAME_SCENE");
-        #         cb.add_command("MAT_DIFFUSE", 0, 1.0, 0.0, 1.0, 1.0);
-        #         cb.add_command("MAT_SPECULAR", 0, 1.0, 1.0, 1.0, 0.0);
-        #         cb.add_command("MAT_EMISSIVE", 0, 0.0, 0.0, 0.0, 0.0);
-        #         cb.add_command("MAT_GLOSSINESS", 0, 30.0);
-        #         cb.add_command("APERTURE", 0.0);
-        #         cb.add_command("EXPOSURE", 0.75);
-        #         cb.add_command("DENSITY", 100.0)
-        #         buf = cb.make_buffer()
-        #         self.send(buf, True)
+    def loop_zstack(self, ch, r, g, b, window, level, prefix='', offset=0):
+        # z-stack test
+        outfilepath = OUTROOT + 'combined_frame_00.ome.tiff'
+        nslices = 64
+        for i in range(0, nslices):
+            cb = CommandBuffer()
+            if i == 0:
+                cb.add_command("LOAD_OME_TIF", outfilepath)
+                cb.add_command("SET_RESOLUTION", 1024, 768)
+                cb.add_command("RENDER_ITERATIONS", 256)
+            cb.add_command("SET_CLIP_REGION", 0, 1, 0, 1, (nslices-1-i)*(1.0/float(nslices)), (nslices-1-i+1)*(1.0/float(nslices)))
+            cb.add_command("EYE", 0.5, 0.333333, 2.52538)
+            cb.add_command("TARGET", 0.5, 0.333333, 0.0952381)
+            cb.add_command("UP", 0, 1, 0)
+            cb.add_command("FOV_Y", 19)
+            cb.add_command("EXPOSURE", 0.2966)
+            cb.add_command("DENSITY", 8.4)
+            cb.add_command("APERTURE", 0)
 
+            nch = 4
+            for c in range(0, nch):
+                cb.add_command("ENABLE_CHANNEL", c, 1 if ch == c else 0)
+                if ch == c:
+                    cb.add_command("MAT_DIFFUSE", c, r, g, b, 1.0)
+                    cb.add_command("MAT_SPECULAR", c, 0, 0, 0, 0.0)
+                    cb.add_command("MAT_EMISSIVE", c, 0, 0, 0, 0.0)
+                    cb.add_command("MAT_GLOSSINESS", c, 0)
+                    cb.add_command("SET_WINDOW_LEVEL", c, window, level)
+
+            cb.add_command("ENABLE_CHANNEL", 4, 1)
+            cb.add_command("MAT_DIFFUSE", 4, 1, 1, 1, 1.0)
+            cb.add_command("MAT_SPECULAR", 4, 0, 0, 0, 0.0)
+            cb.add_command("MAT_EMISSIVE", 4, 0, 0, 0, 0.0)
+            cb.add_command("MAT_GLOSSINESS", 4, 0)
+            cb.add_command("SET_WINDOW_LEVEL", 4, 1, 0.1443)
+
+            cb.add_command("SKYLIGHT_TOP_COLOR", 1, 0, 0)
+            cb.add_command("SKYLIGHT_MIDDLE_COLOR", 1, 1, 1)
+            cb.add_command("SKYLIGHT_BOTTOM_COLOR", 0, 0, 1)
+            cb.add_command("LIGHT_POS", 0, 10, 0, 0)
+            cb.add_command("LIGHT_COLOR", 0, 100, 100, 100)
+            cb.add_command("LIGHT_SIZE", 0, 1, 1)
+            self.push_request(cb, OUTROOT + 'ZSTACK_' + prefix + '_' + str(i+offset).zfill(4) + ".png")
+
+    def loop_frames(self, offset=0):
+        # interp = vtk.vtkCameraInterpolator()
+        # interp.AddCamera(0.0, self.getCameraCopy())
         for i in range(0, 20):
             outfilepath = OUTROOT + 'combined_frame_' + str(i).zfill(2) + '.ome.tiff'
 
@@ -149,42 +178,48 @@ class DummyClient(WebSocketClient):
 
             # test clipping
             cb.add_command("SET_CLIP_REGION", 0, 1, 0, 1, 0, 0.64)
-            cb.add_command("EYE", 0.52469, -0.338348, 0.779557)
-            cb.add_command("TARGET", 0.485269, 0.325739, 0.0273235)
-            cb.add_command("UP", 0.0346096, 0.750108, 0.660407)
+            cb.add_command("EYE", 0.576395, -0.264583, 0.591844)
+            cb.add_command("TARGET", 0.49984, 0.297143, 0.0471128)
+            cb.add_command("UP", 0.0994353, 0.699662, 0.707519)
             cb.add_command("FOV_Y", 55)
-            cb.add_command("EXPOSURE", 0.7655)
-            cb.add_command("DENSITY", 12.8009)
+            cb.add_command("EXPOSURE", 0.35)
+            cb.add_command("DENSITY", 13.8401)
             cb.add_command("APERTURE", 0)
             cb.add_command("ENABLE_CHANNEL", 0, 1)
-            cb.add_command("MAT_DIFFUSE", 0, 1, 1, 1, 1.0)
-            cb.add_command("MAT_SPECULAR", 0, 1, 1, 1, 0.0)
+            cb.add_command("MAT_DIFFUSE", 0, 0.333333, 1, 1, 1.0)
+            cb.add_command("MAT_SPECULAR", 0, 0, 0, 0, 0.0)
             cb.add_command("MAT_EMISSIVE", 0, 0, 0, 0, 0.0)
-            cb.add_command("MAT_GLOSSINESS", 0, 100)
-            cb.add_command("SET_WINDOW_LEVEL", 0, 0.4156, 0.2355)
+            cb.add_command("MAT_GLOSSINESS", 0, 0)
+            cb.add_command("SET_WINDOW_LEVEL", 0, 0.145098, 0.072549)
             cb.add_command("ENABLE_CHANNEL", 1, 1)
-            cb.add_command("MAT_DIFFUSE", 1, 1, 1, 0, 1.0)
-            cb.add_command("MAT_SPECULAR", 1, 1, 1, 0, 0.0)
+            cb.add_command("MAT_DIFFUSE", 1, 1, 0.666667, 1, 1.0)
+            cb.add_command("MAT_SPECULAR", 1, 0, 0, 0, 0.0)
             cb.add_command("MAT_EMISSIVE", 1, 0, 0, 0, 0.0)
-            cb.add_command("MAT_GLOSSINESS", 1, 100)
-            cb.add_command("SET_WINDOW_LEVEL", 1, 0.694118, 0.358824)
+            cb.add_command("MAT_GLOSSINESS", 1, 0)
+            cb.add_command("SET_WINDOW_LEVEL", 1, 0.0117647, 0.00588235)
             cb.add_command("ENABLE_CHANNEL", 2, 1)
-            cb.add_command("MAT_DIFFUSE", 2, 0.333333, 1, 1, 1.0)
+            cb.add_command("MAT_DIFFUSE", 2, 1, 1, 0, 1.0)
             cb.add_command("MAT_SPECULAR", 2, 0, 0, 0, 0.0)
             cb.add_command("MAT_EMISSIVE", 2, 0, 0, 0, 0.0)
             cb.add_command("MAT_GLOSSINESS", 2, 0)
-            cb.add_command("SET_WINDOW_LEVEL", 2, 0.6784, 0.3398)
+            cb.add_command("SET_WINDOW_LEVEL", 2, 0.137255, 0.0686275)
             cb.add_command("ENABLE_CHANNEL", 3, 1)
-            cb.add_command("MAT_DIFFUSE", 3, 0.45098, 0.364706, 0.4, 1.0)
+            cb.add_command("MAT_DIFFUSE", 3, 1, 0.333333, 0, 1.0)
             cb.add_command("MAT_SPECULAR", 3, 0, 0, 0, 0.0)
             cb.add_command("MAT_EMISSIVE", 3, 0, 0, 0, 0.0)
             cb.add_command("MAT_GLOSSINESS", 3, 0)
-            cb.add_command("SET_WINDOW_LEVEL", 3, 0.5725, 0.2833)
+            cb.add_command("SET_WINDOW_LEVEL", 3, 0.0745098, 0.0372549)
+            cb.add_command("ENABLE_CHANNEL", 4, 0)
+            cb.add_command("MAT_DIFFUSE", 4, 1, 1, 1, 1.0)
+            cb.add_command("MAT_SPECULAR", 4, 0, 0, 0, 0.0)
+            cb.add_command("MAT_EMISSIVE", 4, 0, 0, 0, 0.0)
+            cb.add_command("MAT_GLOSSINESS", 4, 0)
+            cb.add_command("SET_WINDOW_LEVEL", 4, 1, 0.2801)
             cb.add_command("SKYLIGHT_TOP_COLOR", 1, 0, 0)
             cb.add_command("SKYLIGHT_MIDDLE_COLOR", 1, 1, 1)
             cb.add_command("SKYLIGHT_BOTTOM_COLOR", 0, 0, 1)
-            cb.add_command("LIGHT_POS", 0, 1, 0, 0)
-            cb.add_command("LIGHT_COLOR", 0, 10, 10, 10)
+            cb.add_command("LIGHT_POS", 0, 10, 0, 0)
+            cb.add_command("LIGHT_COLOR", 0, 100, 100, 100)
             cb.add_command("LIGHT_SIZE", 0, 1, 1)
 
             # orig
@@ -219,27 +254,120 @@ class DummyClient(WebSocketClient):
             # cb.add_command("MAT_EMISSIVE", 3, 0, 0, 0, 0.0)
             # cb.add_command("MAT_GLOSSINESS", 3, 0)
             # cb.add_command("SET_WINDOW_LEVEL", 3, 0.572549, 0.294118)
-            buf = cb.make_buffer()
-            self.send(buf, True)
+
+            self.push_request(cb, OUTROOT + 'ZSTACK__' + str(i+offset).zfill(4) + ".png")
+
+    def testrender(self):
+        cb = CommandBuffer()
+        cb.add_command("LOAD_OME_TIF", "//allen/aics/animated-cell/Dan/2018-02-14_dan_vday_mitosis/timelapse_wt2_s2/combined_frame_00.ome.tiff")
+        cb.add_command("SET_RESOLUTION", 1109, 707)
+        cb.add_command("RENDER_ITERATIONS", 256)
+        cb.add_command("SET_CLIP_REGION", 0, 1, 0, 1, 0, 0.48)
+        cb.add_command("EYE", 0.5, 0.333333, 0.809976)
+        cb.add_command("TARGET", 0.5, 0.333333, 0.0952381)
+        cb.add_command("UP", 0, 1, 0)
+        cb.add_command("FOV_Y", 55)
+        cb.add_command("EXPOSURE", 0.7172)
+        cb.add_command("DENSITY", 50)
+        cb.add_command("APERTURE", 0)
+        cb.add_command("ENABLE_CHANNEL", 0, 1)
+        cb.add_command("MAT_DIFFUSE", 0, 1, 0, 0, 1.0)
+        cb.add_command("MAT_SPECULAR", 0, 0, 0, 0, 0.0)
+        cb.add_command("MAT_EMISSIVE", 0, 0, 0, 0, 0.0)
+        cb.add_command("MAT_GLOSSINESS", 0, 0)
+        cb.add_command("SET_WINDOW_LEVEL", 0, 0.415686, 0.211765)
+        cb.add_command("ENABLE_CHANNEL", 1, 1)
+        cb.add_command("MAT_DIFFUSE", 1, 0, 0.291844, 1, 1.0)
+        cb.add_command("MAT_SPECULAR", 1, 0, 0, 0, 0.0)
+        cb.add_command("MAT_EMISSIVE", 1, 0, 0, 0, 0.0)
+        cb.add_command("MAT_GLOSSINESS", 1, 0)
+        cb.add_command("SET_WINDOW_LEVEL", 1, 0.694118, 0.358824)
+        cb.add_command("ENABLE_CHANNEL", 2, 1)
+        cb.add_command("MAT_DIFFUSE", 2, 0.583673, 1, 0, 1.0)
+        cb.add_command("MAT_SPECULAR", 2, 0, 0, 0, 0.0)
+        cb.add_command("MAT_EMISSIVE", 2, 0, 0, 0, 0.0)
+        cb.add_command("MAT_GLOSSINESS", 2, 0)
+        cb.add_command("SET_WINDOW_LEVEL", 2, 0.678431, 0.343137)
+        cb.add_command("ENABLE_CHANNEL", 3, 1)
+        cb.add_command("MAT_DIFFUSE", 3, 1, 0.537255, 0.00784314, 1.0)
+        cb.add_command("MAT_SPECULAR", 3, 0, 0, 0, 0.0)
+        cb.add_command("MAT_EMISSIVE", 3, 0, 0, 0, 0.0)
+        cb.add_command("MAT_GLOSSINESS", 3, 0)
+        cb.add_command("SET_WINDOW_LEVEL", 3, 0.572549, 0.294118)
+        cb.add_command("ENABLE_CHANNEL", 4, 0)
+        cb.add_command("MAT_DIFFUSE", 4, 0, 1, 0.832837, 1.0)
+        cb.add_command("MAT_SPECULAR", 4, 0, 0, 0, 0.0)
+        cb.add_command("MAT_EMISSIVE", 4, 0, 0, 0, 0.0)
+        cb.add_command("MAT_GLOSSINESS", 4, 0)
+        cb.add_command("SET_WINDOW_LEVEL", 4, 0.321569, 0.164706)
+        cb.add_command("SKYLIGHT_TOP_COLOR", 1, 0, 0)
+        cb.add_command("SKYLIGHT_MIDDLE_COLOR", 1, 1, 1)
+        cb.add_command("SKYLIGHT_BOTTOM_COLOR", 0, 0, 1)
+        cb.add_command("LIGHT_POS", 0, 10, 0, 0)
+        cb.add_command("LIGHT_COLOR", 0, 100, 100, 100)
+        cb.add_command("LIGHT_SIZE", 0, 1, 1)
+        self.push_request(cb, OUTROOT + 'TESTRENDER.png')
+
+    def opened(self):
+        self.queue = deque([])
+        print("opened up")
+
+        # 00 .. 19
+        # for j in range(0, len(CHNAMES)):
+        #     for i in range(0, 20):
+        #         outfilepath = OUTROOT + CHNAMES[j] + '_frame_' + str(i).zfill(2) + '.ome.tiff'
+        #
+        #         cb = CommandBuffer()
+        #         cb.add_command("LOAD_OME_TIF", outfilepath)
+        #
+        #         # render
+        #         cb.add_command("SET_RESOLUTION", 1024, 1024)
+        #         cb.add_command("RENDER_ITERATIONS", 64)
+        #         cb.add_command("FRAME_SCENE");
+        #         cb.add_command("MAT_DIFFUSE", 0, 1.0, 0.0, 1.0, 1.0);
+        #         cb.add_command("MAT_SPECULAR", 0, 1.0, 1.0, 1.0, 0.0);
+        #         cb.add_command("MAT_EMISSIVE", 0, 0.0, 0.0, 0.0, 0.0);
+        #         cb.add_command("MAT_GLOSSINESS", 0, 30.0);
+        #         cb.add_command("APERTURE", 0.0);
+        #         cb.add_command("EXPOSURE", 0.75);
+        #         cb.add_command("DENSITY", 100.0)
+        #         buf = cb.make_buffer()
+        #         self.send(buf, True)
+
+        # self.testrender()
+
+        self.loop_zstack(0, 0.333333, 1, 1, 0.145098, 0.072549, offset=0)
+        self.loop_zstack(1, 1, 0.666667, 1, 0.0117647, 0.00588235, offset=64)
+        self.loop_zstack(2, 1, 1, 0, 0.137255, 0.0686275, offset=128)
+        self.loop_zstack(3, 1, 0.333333, 0, 0.0745098, 0.0372549, offset=192)
+
+        self.loop_frames(offset=256)
+        self.loop_frames(offset=276)
+        self.loop_frames(offset=296)
+
+
 
     def closed(self, code, reason=None):
         print("Closed down", code, reason)
 
     def received_message(self, m):
         if m.is_binary:
-            global N
+            name = self.queue.popleft()
             im = Image.open(io.BytesIO(m.data))
-            im.save(OUTROOT + "Test000" + "_" + str(N%20).zfill(2) + ".png")
-            N=N+1
+            im.save(name)
+
+            # imgplot.set_data(im)
         else:
             print(m)
             if len(m) == 175:
                 self.close(reason='Bye bye')
 
+# imgplot = plt.imshow(numpy.zeros((1024, 768)))
 if __name__ == '__main__':
     try:
         # convert_combined()
         # convertFiles()
+        # ws = DummyClient('ws://dev-aics-dtp-001:1235/', protocols=['http-only', 'chat'])
         ws = DummyClient('ws://localhost:1235/', protocols=['http-only', 'chat'])
         ws.connect()
         ws.run_forever()
