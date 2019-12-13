4c6943bac42eaa4962cc41eb74eb8864ed0a6a9d
Daniel Toloudis <danielt@alleninstitute.org>
2019-12-13 14:11:49 -0800

    actually run the script path from the command line

--- qtome/python/ScriptServer.cpp
+++ qtome/python/ScriptServer.cpp
@@ -5,6 +5,16 @@
 #include "pybind11/embed.h" // everything needed for embedding
 namespace py = pybind11;
 
+const std::string testScript = R"(
+    import agave
+    r = agave.renderer()
+    r.load_ome_tif("E:\\data\\AICS-12_881_7.ome.tif")
+    r.render_iterations(100)
+    r.frame_scene()
+    r.session("E:\\test.png")
+    r.redraw()
+  )";
+
 ScriptServer::ScriptServer() {}
 
 ScriptServer::~ScriptServer() {}
@@ -18,18 +28,9 @@ ScriptServer::runScriptFile(const std::string& path)
 
   // use the Python API
   py::print("Hello, World!");
-  //  py::eval_file(path, globals);
   try {
-    py::exec(R"(
-    import agave
-    r = agave.renderer()
-    r.load_ome_tif("E:\\data\\AICS-12_881_7.ome.tif")
-    r.render_iterations(100)
-    r.frame_scene()
-    r.session("E:\\test.png")
-    r.redraw()
-  )",
-             globals);
+    py::eval_file(path, globals);
+    // py::exec(testScript, globals);
   } catch (py::error_already_set& ex) {
     py::print(ex.what());
     LOG_ERROR << ex.what();
