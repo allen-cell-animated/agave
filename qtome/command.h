5152bfff4db0a41481acd83b7f0255c54d689f4c
Dan Toloudis <dmt.reg@gmail.com>
2020-01-03 16:34:24 -0500

    prefer "enum class" over "enum"

--- qtome/command.h
+++ qtome/command.h
@@ -16,7 +16,7 @@ public:
   virtual std::string parseString() = 0;
 };
 
-enum CommandArgType
+enum class CommandArgType
 {
   I32,
   F32,
@@ -71,43 +71,46 @@ struct SessionCommandD
 {
   std::string m_name;
 };
-CMDDECL(SessionCommand, 0, "session", CMD_ARGS({ STR }));
+CMDDECL(SessionCommand, 0, "session", CMD_ARGS({ CommandArgType::STR }));
 
 struct AssetPathCommandD
 {
   std::string m_name;
 };
-CMDDECL(AssetPathCommand, 1, "asset_path", CMD_ARGS({ STR }));
+CMDDECL(AssetPathCommand, 1, "asset_path", CMD_ARGS({ CommandArgType::STR }));
 
 struct LoadOmeTifCommandD
 {
   std::string m_name;
 };
-CMDDECL(LoadOmeTifCommand, 2, "load_ome_tif", CMD_ARGS({ STR }));
+CMDDECL(LoadOmeTifCommand, 2, "load_ome_tif", CMD_ARGS({ CommandArgType::STR }));
 
 struct SetCameraPosCommandD
 {
   float m_x, m_y, m_z;
 };
-CMDDECL(SetCameraPosCommand, 3, "eye", CMD_ARGS({ F32, F32, F32 }));
+CMDDECL(SetCameraPosCommand, 3, "eye", CMD_ARGS({ CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetCameraTargetCommandD
 {
   float m_x, m_y, m_z;
 };
-CMDDECL(SetCameraTargetCommand, 4, "target", CMD_ARGS({ F32, F32, F32 }));
+CMDDECL(SetCameraTargetCommand,
+        4,
+        "target",
+        CMD_ARGS({ CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetCameraUpCommandD
 {
   float m_x, m_y, m_z;
 };
-CMDDECL(SetCameraUpCommand, 5, "up", CMD_ARGS({ F32, F32, F32 }));
+CMDDECL(SetCameraUpCommand, 5, "up", CMD_ARGS({ CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetCameraApertureCommandD
 {
   float m_x;
 };
-CMDDECL(SetCameraApertureCommand, 6, "aperture", CMD_ARGS({ F32 }));
+CMDDECL(SetCameraApertureCommand, 6, "aperture", CMD_ARGS({ CommandArgType::F32 }));
 
 struct SetCameraProjectionCommandD
 {
@@ -116,52 +119,64 @@ struct SetCameraProjectionCommandD
   // fov degrees or ortho scale
   float m_x;
 };
-CMDDECL(SetCameraProjectionCommand, 7, "camera_projection", CMD_ARGS({ I32, F32 }));
+CMDDECL(SetCameraProjectionCommand, 7, "camera_projection", CMD_ARGS({ CommandArgType::I32, CommandArgType::F32 }));
 
 struct SetCameraFocalDistanceCommandD
 {
   float m_x;
 };
-CMDDECL(SetCameraFocalDistanceCommand, 8, "focaldist", CMD_ARGS({ F32 }));
+CMDDECL(SetCameraFocalDistanceCommand, 8, "focaldist", CMD_ARGS({ CommandArgType::F32 }));
 
 struct SetCameraExposureCommandD
 {
   float m_x;
 };
-CMDDECL(SetCameraExposureCommand, 9, "exposure", CMD_ARGS({ F32 }));
+CMDDECL(SetCameraExposureCommand, 9, "exposure", CMD_ARGS({ CommandArgType::F32 }));
 
 struct SetDiffuseColorCommandD
 {
   int32_t m_channel;
   float m_r, m_g, m_b, m_a;
 };
-CMDDECL(SetDiffuseColorCommand, 10, "mat_diffuse", CMD_ARGS({ I32, F32, F32, F32, F32 }));
+CMDDECL(SetDiffuseColorCommand,
+        10,
+        "mat_diffuse",
+        CMD_ARGS(
+          { CommandArgType::I32, CommandArgType::F32, CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetSpecularColorCommandD
 {
   int32_t m_channel;
   float m_r, m_g, m_b, m_a;
 };
-CMDDECL(SetSpecularColorCommand, 11, "mat_specular", CMD_ARGS({ I32, F32, F32, F32, F32 }));
+CMDDECL(SetSpecularColorCommand,
+        11,
+        "mat_specular",
+        CMD_ARGS(
+          { CommandArgType::I32, CommandArgType::F32, CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetEmissiveColorCommandD
 {
   int32_t m_channel;
   float m_r, m_g, m_b, m_a;
 };
-CMDDECL(SetEmissiveColorCommand, 12, "mat_emissive", CMD_ARGS({ I32, F32, F32, F32, F32 }));
+CMDDECL(SetEmissiveColorCommand,
+        12,
+        "mat_emissive",
+        CMD_ARGS(
+          { CommandArgType::I32, CommandArgType::F32, CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetRenderIterationsCommandD
 {
   int32_t m_x;
 };
-CMDDECL(SetRenderIterationsCommand, 13, "render_iterations", CMD_ARGS({ I32 }));
+CMDDECL(SetRenderIterationsCommand, 13, "render_iterations", CMD_ARGS({ CommandArgType::I32 }));
 
 struct SetStreamModeCommandD
 {
   int32_t m_x;
 };
-CMDDECL(SetStreamModeCommand, 14, "stream_mode", CMD_ARGS({ I32 }));
+CMDDECL(SetStreamModeCommand, 14, "stream_mode", CMD_ARGS({ CommandArgType::I32 }));
 
 struct RequestRedrawCommandD
 {};
@@ -171,13 +186,13 @@ struct SetResolutionCommandD
 {
   int32_t m_x, m_y;
 };
-CMDDECL(SetResolutionCommand, 16, "set_resolution", CMD_ARGS({ I32, I32 }));
+CMDDECL(SetResolutionCommand, 16, "set_resolution", CMD_ARGS({ CommandArgType::I32, CommandArgType::I32 }));
 
 struct SetDensityCommandD
 {
   float m_x;
 };
-CMDDECL(SetDensityCommand, 17, "density", CMD_ARGS({ F32 }));
+CMDDECL(SetDensityCommand, 17, "density", CMD_ARGS({ CommandArgType::F32 }));
 
 struct FrameSceneCommandD
 {};
@@ -188,14 +203,14 @@ struct SetGlossinessCommandD
   int32_t m_channel;
   float m_glossiness;
 };
-CMDDECL(SetGlossinessCommand, 19, "mat_glossiness", CMD_ARGS({ I32, F32 }));
+CMDDECL(SetGlossinessCommand, 19, "mat_glossiness", CMD_ARGS({ CommandArgType::I32, CommandArgType::F32 }));
 
 struct EnableChannelCommandD
 {
   int32_t m_channel;
   int32_t m_enabled;
 };
-CMDDECL(EnableChannelCommand, 20, "enable_channel", CMD_ARGS({ I32, I32 }));
+CMDDECL(EnableChannelCommand, 20, "enable_channel", CMD_ARGS({ CommandArgType::I32, CommandArgType::I32 }));
 
 struct SetWindowLevelCommandD
 {
@@ -203,53 +218,74 @@ struct SetWindowLevelCommandD
   float m_window;
   float m_level;
 };
-CMDDECL(SetWindowLevelCommand, 21, "set_window_level", CMD_ARGS({ I32, F32, F32 }));
+CMDDECL(SetWindowLevelCommand,
+        21,
+        "set_window_level",
+        CMD_ARGS({ CommandArgType::I32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct OrbitCameraCommandD
 {
   float m_theta;
   float m_phi;
 };
-CMDDECL(OrbitCameraCommand, 22, "orbit_camera", CMD_ARGS({ F32, F32 }));
+CMDDECL(OrbitCameraCommand, 22, "orbit_camera", CMD_ARGS({ CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetSkylightTopColorCommandD
 {
   float m_r, m_g, m_b;
 };
-CMDDECL(SetSkylightTopColorCommand, 23, "skylight_top_color", CMD_ARGS({ F32, F32, F32 }));
+CMDDECL(SetSkylightTopColorCommand,
+        23,
+        "skylight_top_color",
+        CMD_ARGS({ CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetSkylightMiddleColorCommandD
 {
   float m_r, m_g, m_b;
 };
-CMDDECL(SetSkylightMiddleColorCommand, 24, "skylight_middle_color", CMD_ARGS({ F32, F32, F32 }));
+CMDDECL(SetSkylightMiddleColorCommand,
+        24,
+        "skylight_middle_color",
+        CMD_ARGS({ CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetSkylightBottomColorCommandD
 {
   float m_r, m_g, m_b;
 };
-CMDDECL(SetSkylightBottomColorCommand, 25, "skylight_bottom_color", CMD_ARGS({ F32, F32, F32 }));
+CMDDECL(SetSkylightBottomColorCommand,
+        25,
+        "skylight_bottom_color",
+        CMD_ARGS({ CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetLightPosCommandD
 {
   int32_t m_index;
   float m_r, m_theta, m_phi;
 };
-CMDDECL(SetLightPosCommand, 26, "light_pos", CMD_ARGS({ I32, F32, F32, F32 }));
+CMDDECL(SetLightPosCommand,
+        26,
+        "light_pos",
+        CMD_ARGS({ CommandArgType::I32, CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetLightColorCommandD
 {
   int32_t m_index;
   float m_r, m_g, m_b;
 };
-CMDDECL(SetLightColorCommand, 27, "light_color", CMD_ARGS({ I32, F32, F32, F32 }));
+CMDDECL(SetLightColorCommand,
+        27,
+        "light_color",
+        CMD_ARGS({ CommandArgType::I32, CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetLightSizeCommandD
 {
   int32_t m_index;
   float m_x, m_y;
 };
-CMDDECL(SetLightSizeCommand, 28, "light_size", CMD_ARGS({ I32, F32, F32 }));
+CMDDECL(SetLightSizeCommand,
+        28,
+        "light_size",
+        CMD_ARGS({ CommandArgType::I32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetClipRegionCommandD
 {
@@ -257,20 +293,31 @@ struct SetClipRegionCommandD
   float m_miny, m_maxy;
   float m_minz, m_maxz;
 };
-CMDDECL(SetClipRegionCommand, 29, "set_clip_region", CMD_ARGS({ F32, F32, F32, F32, F32, F32 }));
+CMDDECL(SetClipRegionCommand,
+        29,
+        "set_clip_region",
+        CMD_ARGS({ CommandArgType::F32,
+                   CommandArgType::F32,
+                   CommandArgType::F32,
+                   CommandArgType::F32,
+                   CommandArgType::F32,
+                   CommandArgType::F32 }));
 
 struct SetVoxelScaleCommandD
 {
   float m_x, m_y, m_z;
 };
-CMDDECL(SetVoxelScaleCommand, 30, "set_voxel_scale", CMD_ARGS({ F32, F32, F32 }));
+CMDDECL(SetVoxelScaleCommand,
+        30,
+        "set_voxel_scale",
+        CMD_ARGS({ CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct AutoThresholdCommandD
 {
   int32_t m_channel;
   int32_t m_method;
 };
-CMDDECL(AutoThresholdCommand, 31, "auto_threshold", CMD_ARGS({ I32, I32 }));
+CMDDECL(AutoThresholdCommand, 31, "auto_threshold", CMD_ARGS({ CommandArgType::I32, CommandArgType::I32 }));
 
 struct SetPercentileThresholdCommandD
 {
@@ -278,29 +325,35 @@ struct SetPercentileThresholdCommandD
   float m_pctLow;
   float m_pctHigh;
 };
-CMDDECL(SetPercentileThresholdCommand, 32, "set_percentile_threshold", CMD_ARGS({ I32, F32, F32 }));
+CMDDECL(SetPercentileThresholdCommand,
+        32,
+        "set_percentile_threshold",
+        CMD_ARGS({ CommandArgType::I32, CommandArgType::F32, CommandArgType::F32 }));
 
 struct SetOpacityCommandD
 {
   int32_t m_channel;
   float m_opacity;
 };
-CMDDECL(SetOpacityCommand, 33, "mat_opacity", CMD_ARGS({ I32, F32 }));
+CMDDECL(SetOpacityCommand, 33, "mat_opacity", CMD_ARGS({ CommandArgType::I32, CommandArgType::F32 }));
 
 struct SetPrimaryRayStepSizeCommandD
 {
   float m_stepSize;
 };
-CMDDECL(SetPrimaryRayStepSizeCommand, 34, "set_primary_ray_step_size", CMD_ARGS({ F32 }));
+CMDDECL(SetPrimaryRayStepSizeCommand, 34, "set_primary_ray_step_size", CMD_ARGS({ CommandArgType::F32 }));
 
 struct SetSecondaryRayStepSizeCommandD
 {
   float m_stepSize;
 };
-CMDDECL(SetSecondaryRayStepSizeCommand, 35, "set_secondary_ray_step_size", CMD_ARGS({ F32 }));
+CMDDECL(SetSecondaryRayStepSizeCommand, 35, "set_secondary_ray_step_size", CMD_ARGS({ CommandArgType::F32 }));
 
 struct SetBackgroundColorCommandD
 {
   float m_r, m_g, m_b;
 };
-CMDDECL(SetBackgroundColorCommand, 36, "background_color", CMD_ARGS({ F32, F32, F32 }));
+CMDDECL(SetBackgroundColorCommand,
+        36,
+        "background_color",
+        CMD_ARGS({ CommandArgType::F32, CommandArgType::F32, CommandArgType::F32 }));
