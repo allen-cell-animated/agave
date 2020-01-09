7c36fa427960e71635950eb80fda999e16e090d8
Daniel Toloudis <danielt@alleninstitute.org>
2020-01-09 14:45:29 -0800

    clean up ScriptServer and provide implementation for running python code from a string

--- qtome/python/ScriptServer.cpp
+++ qtome/python/ScriptServer.cpp
@@ -5,15 +5,17 @@
 #include "pybind11/embed.h" // everything needed for embedding
 namespace py = pybind11;
 
-const std::string testScript = R"(
-    import agave
-    r = agave.renderer()
-    r.load_ome_tif("E:\\data\\AICS-12_881_7.ome.tif")
-    r.render_iterations(100)
-    r.frame_scene()
-    r.session("E:\\test.png")
-    r.redraw()
-  )";
+// This is here just as an example of the expected syntax,
+// and could be passed to ScriptServer::runScript().
+// const std::string testScript = R"(
+//     import agave
+//     r = agave.renderer()
+//     r.load_ome_tif("AICS-12_881_7.ome.tif")
+//     r.render_iterations(100)
+//     r.frame_scene()
+//     r.session("test.png")
+//     r.redraw()
+//   )";
 
 ScriptServer::ScriptServer() {}
 
@@ -27,7 +29,7 @@ ScriptServer::runScriptFile(const std::string& path)
   auto globals = py::globals();
 
   // use the Python API
-  py::print("Hello, World!");
+  LOG_INFO << "Begin Python script interpreter";
   try {
     py::eval_file(path, globals);
     // py::exec(testScript, globals);
@@ -35,8 +37,23 @@ ScriptServer::runScriptFile(const std::string& path)
     py::print(ex.what());
     LOG_ERROR << ex.what();
   }
+  LOG_INFO << "End Python script interpreter";
 }
 
 void
 ScriptServer::runScript(const std::string& source)
-{}
+{
+  py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+  auto agave_module = py::module::import("agave");
+  auto globals = py::globals();
+
+  // use the Python API
+  LOG_INFO << "Begin Python script interpreter";
+  try {
+    py::exec(source, globals);
+  } catch (py::error_already_set& ex) {
+    py::print(ex.what());
+    LOG_ERROR << ex.what();
+  }
+  LOG_INFO << "End Python script interpreter";
+}
