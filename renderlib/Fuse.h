96af48633ddf1b454796c6fbf54939954b57f862
Dan Toloudis <dmt.reg@gmail.com>
2020-03-10 16:38:28 -0700

    comment

--- renderlib/Fuse.h
+++ renderlib/Fuse.h
@@ -8,11 +8,13 @@
 
 class ImageXYZC;
 
-// if channel color is 0, then channel will not contribute.
-// allocates memory for outRGBVolume and outGradientVolume
+// Runs a processing step that applies a color into each channel,
+// and then combines the channels to result in a single RGB colored volume
 class Fuse
 {
 public:
+  // if channel color is 0, then channel will not contribute.
+  // allocates memory for outRGBVolume and outGradientVolume
   static void fuse(const ImageXYZC* img,
                    const std::vector<glm::vec3>& colorsPerChannel,
                    uint8_t** outRGBVolume,
