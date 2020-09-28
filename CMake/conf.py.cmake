e15631907383df6ee366a6fe98a095621b12aadd
Dan <danielt@alleninstitute.org>
2020-09-28 16:06:12 -0700

    add unified version to docs from CMakeLists master file

--- CMake/conf.py.cmake
+++ CMake/conf.py.cmake
@@ -22,7 +22,7 @@ copyright = '2020, Allen Institute for Cell Science'
 author = 'Allen Institute for Cell Science'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.9.0'
+release = '@PROJECT_VERSION@'
 
 
 # -- General configuration ---------------------------------------------------
