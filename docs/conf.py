c8283debac7507ce97775f92f3d80c910a29ed63
DMT <dmt@completelydifferent.com>
2020-11-19 14:17:35 -0800

    v1.0.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Allen Institute for Cell Science'
 author = 'Allen Institute for Cell Science'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0.0'
+release = '1.0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
