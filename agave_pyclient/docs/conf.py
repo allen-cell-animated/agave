b52b96b27b7bf0261e3fc240644a70fbaad39688
Dan <danielt@alleninstitute.org>
2021-07-20 11:54:59 -0700

    comment out latex options

--- agave_pyclient/docs/conf.py
+++ agave_pyclient/docs/conf.py
@@ -131,33 +131,33 @@ htmlhelp_basename = "agave_pyclientdoc"
 
 # -- Options for LaTeX output ------------------------------------------
 
-latex_elements = {
-    # The paper size ("letterpaper" or "a4paper").
-    #
-    # "papersize": "letterpaper",
-    # The font size ("10pt", "11pt" or "12pt").
-    #
-    # "pointsize": "10pt",
-    # Additional stuff for the LaTeX preamble.
-    #
-    # "preamble": "",
-    # Latex figure (float) alignment
-    #
-    # "figure_align": "htbp",
-}
+# latex_elements = {
+#     # The paper size ("letterpaper" or "a4paper").
+#     #
+#     # "papersize": "letterpaper",
+#     # The font size ("10pt", "11pt" or "12pt").
+#     #
+#     # "pointsize": "10pt",
+#     # Additional stuff for the LaTeX preamble.
+#     #
+#     # "preamble": "",
+#     # Latex figure (float) alignment
+#     #
+#     # "figure_align": "htbp",
+# }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
-latex_documents = [
-    (
-        master_doc,
-        "agave_pyclient.tex",
-        u"agave_pyclient Documentation",
-        u"Daniel Toloudis",
-        "manual",
-    ),
-]
+# latex_documents = [
+#     (
+#         master_doc,
+#         "agave_pyclient.tex",
+#         u"agave_pyclient Documentation",
+#         u"Daniel Toloudis",
+#         "manual",
+#     ),
+# ]
 
 
 # -- Options for manual page output ------------------------------------
