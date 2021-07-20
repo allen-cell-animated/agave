fe11c8b16fa0175ee0a36c09deed2a2920c89d0f
Dan <danielt@alleninstitute.org>
2021-07-20 11:41:19 -0700

    add numpy to requirements

--- agave_pyclient/setup.py
+++ agave_pyclient/setup.py
@@ -37,7 +37,7 @@ dev_requirements = [
     "wheel>=0.34.2",
 ]
 
-requirements = ["ws4py==0.5.1", "Pillow==8.2.0"]
+requirements = ["numpy", "ws4py==0.5.1", "Pillow==8.2.0"]
 
 extra_requirements = {
     "setup": setup_requirements,
