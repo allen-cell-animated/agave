be09cc5531dfa423d4192ddd5af935989eefe763
Dan <danielt@alleninstitute.org>
2021-07-20 11:10:17 -0700

    add import back in

--- agave_pyclient/docs/conf.py
+++ agave_pyclient/docs/conf.py
@@ -24,6 +24,7 @@ import sys
 import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath(".."))
+import agave_pyclient
 
 
 # -- General configuration ---------------------------------------------
