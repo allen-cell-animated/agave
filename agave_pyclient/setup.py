1a6a31f3973b71197bf3e77a802c18b818c2e17b
dmt <dmt@completelydifferent.com>
2021-08-11 20:17:37 -0700

    Bump to 1.0.6

--- agave_pyclient/setup.py
+++ agave_pyclient/setup.py
@@ -84,6 +84,6 @@ setup(
     url="https://github.com/allen-cell-animated/agave",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.0.5",
+    version="1.0.6",
     zip_safe=False,
 )
