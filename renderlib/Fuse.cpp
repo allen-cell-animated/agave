8a874fff96a6ecef992a9fdea549bfc1928d03e5
Dan <danielt@alleninstitute.org>
2021-11-24 15:39:14 -0800

    shuffling code around and trying different ways to signal back and forth

--- renderlib/Fuse.cpp
+++ renderlib/Fuse.cpp
@@ -3,340 +3,10 @@
 #include "ImageXYZC.h"
 
 #include <algorithm>
-#include <atomic>
-#include <future>
+//#include <future>
 #include <iostream>
-#include <queue>
-#include <stdexcept>
-#include <thread>
-#include <type_traits>
-
-template<typename T>
-class unbounded_queue
-{
-public:
-  explicit unbounded_queue(bool block = true)
-    : m_block{ block }
-  {}
-
-  void push(const T& item)
-  {
-    {
-      std::scoped_lock guard(m_queue_lock);
-      m_queue.push(item);
-    }
-    m_condition.notify_one();
-  }
-
-  void push(T&& item)
-  {
-    {
-      std::scoped_lock guard(m_queue_lock);
-      m_queue.push(std::move(item));
-    }
-    m_condition.notify_one();
-  }
-
-  template<typename... Args>
-  void emplace(Args&&... args)
-  {
-    {
-      std::scoped_lock guard(m_queue_lock);
-      m_queue.emplace(std::forward<Args>(args)...);
-    }
-    m_condition.notify_one();
-  }
-
-  bool try_push(const T& item)
-  {
-    {
-      std::unique_lock lock(m_queue_lock, std::try_to_lock);
-      if (!lock)
-        return false;
-      m_queue.push(item);
-    }
-    m_condition.notify_one();
-    return true;
-  }
-
-  bool try_push(T&& item)
-  {
-    {
-      std::unique_lock lock(m_queue_lock, std::try_to_lock);
-      if (!lock)
-        return false;
-      m_queue.push(std::move(item));
-    }
-    m_condition.notify_one();
-    return true;
-  }
-
-  bool pop(T& item)
-  {
-    std::unique_lock guard(m_queue_lock);
-    m_condition.wait(guard, [&]() { return !m_queue.empty() || !m_block; });
-    if (m_queue.empty())
-      return false;
-    item = std::move(m_queue.front());
-    m_queue.pop();
-    return true;
-  }
-
-  bool try_pop(T& item)
-  {
-    std::unique_lock lock(m_queue_lock, std::try_to_lock);
-    if (!lock || m_queue.empty())
-      return false;
-    item = std::move(m_queue.front());
-    m_queue.pop();
-    return true;
-  }
-
-  std::size_t size() const
-  {
-    std::scoped_lock guard(m_queue_lock);
-    return m_queue.size();
-  }
-
-  bool empty() const
-  {
-    std::scoped_lock guard(m_queue_lock);
-    return m_queue.empty();
-  }
-
-  void block()
-  {
-    std::scoped_lock guard(m_queue_lock);
-    m_block = true;
-  }
-
-  void unblock()
-  {
-    {
-      std::scoped_lock guard(m_queue_lock);
-      m_block = false;
-    }
-    m_condition.notify_all();
-  }
-
-  bool blocking() const
-  {
-    std::scoped_lock guard(m_queue_lock);
-    return m_block;
-  }
-
-private:
-  using queue_t = std::queue<T>;
-  queue_t m_queue;
-
-  bool m_block;
-
-  mutable std::mutex m_queue_lock;
-  std::condition_variable m_condition;
-};
-
-template<typename T>
-class bounded_queue
-{
-public:
-  explicit bounded_queue(std::size_t max_size, bool block = true)
-    : m_block{ block }
-    , m_max_size{ max_size }
-  {
-    if (!m_max_size)
-      throw std::invalid_argument("bad queue max-size! must be non-zero!");
-  }
-
-  bool push(const T& item)
-  {
-    {
-      std::unique_lock guard(m_queue_lock);
-      m_condition_push.wait(guard, [&]() { return m_queue.size() < m_max_size || !m_block; });
-      if (m_queue.size() == m_max_size)
-        return false;
-      m_queue.push(item);
-    }
-    m_condition_pop.notify_one();
-    return true;
-  }
-
-  bool push(T&& item)
-  {
-    {
-      std::unique_lock guard(m_queue_lock);
-      m_condition_push.wait(guard, [&]() { return m_queue.size() < m_max_size || !m_block; });
-      if (m_queue.size() == m_max_size)
-        return false;
-      m_queue.push(std::move(item));
-    }
-    m_condition_pop.notify_one();
-    return true;
-  }
-
-  template<typename... Args>
-  bool emplace(Args&&... args)
-  {
-    {
-      std::unique_lock guard(m_queue_lock);
-      m_condition_push.wait(guard, [&]() { return m_queue.size() < m_max_size || !m_block; });
-      if (m_queue.size() == m_max_size)
-        return false;
-      m_queue.emplace(std::forward<Args>(args)...);
-    }
-    m_condition_pop.notify_one();
-    return true;
-  }
-
-  bool pop(T& item)
-  {
-    {
-      std::unique_lock guard(m_queue_lock);
-      m_condition_pop.wait(guard, [&]() { return !m_queue.empty() || !m_block; });
-      if (m_queue.empty())
-        return false;
-      item = std::move(m_queue.front());
-      m_queue.pop();
-    }
-    m_condition_push.notify_one();
-    return true;
-  }
-
-  std::size_t size() const
-  {
-    std::scoped_lock guard(m_queue_lock);
-    return m_queue.size();
-  }
-
-  std::size_t capacity() const { return m_max_size; }
-
-  bool empty() const
-  {
-    std::scoped_lock guard(m_queue_lock);
-    return m_queue.empty();
-  }
-
-  bool full() const
-  {
-    std::scoped_lock lock(m_queue_lock);
-    return m_queue.size() == capacity();
-  }
-
-  void block()
-  {
-    std::scoped_lock guard(m_queue_lock);
-    m_block = true;
-  }
-
-  void unblock()
-  {
-    {
-      std::scoped_lock guard(m_queue_lock);
-      m_block = false;
-    }
-    m_condition_push.notify_all();
-    m_condition_pop.notify_all();
-  }
-
-  bool blocking() const
-  {
-    std::scoped_lock guard(m_queue_lock);
-    return m_block;
-  }
-
-private:
-  using queue_t = std::queue<T>;
-  queue_t m_queue;
-
-  bool m_block;
-  const std::size_t m_max_size;
-
-  mutable std::mutex m_queue_lock;
-  std::condition_variable m_condition_push;
-  std::condition_variable m_condition_pop;
-};
-
-class thread_pool
-{
-public:
-  explicit thread_pool(std::size_t thread_count = std::thread::hardware_concurrency())
-    : m_queues(thread_count)
-    , m_count(thread_count)
-  {
-    if (!thread_count)
-      throw std::invalid_argument("bad thread count! must be non-zero!");
-
-    auto worker = [this](auto i) {
-      while (true) {
-        proc_t f;
-        for (auto n = 0; n < m_count * K; ++n)
-          if (m_queues[(i + n) % m_count].try_pop(f))
-            break;
-        if (!f && !m_queues[i].pop(f))
-          break;
-        f();
-      }
-    };
-
-    m_threads.reserve(thread_count);
-    for (auto i = 0; i < thread_count; ++i)
-      m_threads.emplace_back(worker, i);
-  }
-
-  ~thread_pool()
-  {
-    for (auto& queue : m_queues)
-      queue.unblock();
-    for (auto& thread : m_threads)
-      thread.join();
-  }
-
-  template<typename F, typename... Args>
-  void enqueue_work(F&& f, Args&&... args)
-  {
-    auto work = [p = std::forward<F>(f), t = std::make_tuple(std::forward<Args>(args)...)]() { std::apply(p, t); };
-    auto i = m_index++;
-
-    for (auto n = 0; n < m_count * K; ++n)
-      if (m_queues[(i + n) % m_count].try_push(work))
-        return;
-
-    m_queues[i % m_count].push(std::move(work));
-  }
-
-  template<typename F, typename... Args>
-  [[nodiscard]] auto enqueue_task(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>>
-  {
-    using task_return_type = std::invoke_result_t<F, Args...>;
-    using task_type = std::packaged_task<task_return_type()>;
-
-    auto task = std::make_shared<task_type>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-    auto work = [=]() { (*task)(); };
-    auto result = task->get_future();
-    auto i = m_index++;
-
-    for (auto n = 0; n < m_count * K; ++n)
-      if (m_queues[(i + n) % m_count].try_push(work))
-        return result;
-
-    m_queues[i % m_count].push(std::move(work));
-
-    return result;
-  }
-
-private:
-  using proc_t = std::function<void(void)>;
-  using queue_t = unbounded_queue<proc_t>;
-  using queues_t = std::vector<queue_t>;
-  queues_t m_queues;
-
-  using threads_t = std::vector<std::thread>;
-  threads_t m_threads;
-
-  const std::size_t m_count;
-  std::atomic_uint m_index = 0;
-
-  inline static const unsigned int K = 2;
-};
+//#include <stdexcept>
+//#include <type_traits>
 
 const bool Fuse::FUSE_THREADED = true;
 const size_t Fuse::NTHREADS = 1;
@@ -344,18 +14,15 @@ const size_t Fuse::NTHREADS = 1;
 Fuse::Fuse()
   : m_img(nullptr)
   , m_outRGBVolume(nullptr)
+  , m_stop(false)
+  , m_fuseRequests(1)
 {}
 
 Fuse::~Fuse()
 {
-  {
-    std::lock_guard<std::mutex> lock(m_mutex);
-    m_stop = true;
-  }
-  m_conditionVar.notify_all();
-  for (auto& thread : m_threads) {
+  m_fuseRequests.unblock();
+  for (auto& thread : m_threads)
     thread.join();
-  }
 }
 
 void
@@ -375,22 +42,50 @@ Fuse::init(const ImageXYZC* img, uint8_t* outRGBVolume)
 void
 Fuse::fuseThreadWorker(size_t whichThread, size_t nThreads)
 {
-  std::cout << "THREAD " << whichThread << " LAUNCHED" << std::endl;
-  // lock to check value of m_stop
-  std::unique_lock<std::mutex> lock(m_mutex);
-  while (!m_stop) {
-    std::cout << "THREAD " << whichThread << " IN LOOP, WAITING" << std::endl;
-    // wait but relinquish the lock back to main thread
-    // m_conditionVar.wait(lock, check_for_fuse_work_or_stopped);
-    m_conditionVar.wait(lock);
+  static const int K = 2;
+  while (true) {
+    std::vector<glm::vec3> data;
+    for (auto n = 0; n < nThreads * K; ++n)
+      if (m_fuseRequests.try_pop(data))
+        break;
+    if (data.empty() && !m_fuseRequests.pop(data))
+      break;
 
-    std::cout << "THREAD " << whichThread << " SIGNALLED, RUNNING" << std::endl;
-    //    if (hasNewFuseTask) {
-    //      doFuse(whichThread, nThreads, colorsPerChannel);
-    // m_nThreadsWorking--;
-    // check for doneness and then check for ONE outstanding fuse request
-    //    }
+    std::cout << "THREAD " << whichThread << " GOT DATA, RUNNING" << std::endl;
+
+    doFuse(whichThread, nThreads, data);
   }
+
+  std::cout << "THREAD " << whichThread << " LAUNCHED" << std::endl;
+  // // lock to check value of m_stop
+  // std::unique_lock<std::mutex> lock(m_mutex);
+  // while (!m_stop) {
+  //   std::cout << "THREAD " << whichThread << " IN LOOP, WAITING" << std::endl;
+  //   // wait but relinquish the lock back to main thread
+  //   m_conditionVar.wait(lock, [this] { return !m_queuedColorsToFuse.empty(); });
+
+  //   std::cout << "THREAD " << whichThread << " SIGNALLED, RUNNING" << std::endl;
+  //   m_nThreadsWorking++;
+  //   doFuse(whichThread, nThreads, m_queuedColorsToFuse);
+  //   m_nThreadsWorking--;
+  //   if (m_nThreadsWorking == 0) {
+  //     // i am the last thread to complete: so signal back to main thread that we have something ready?
+  //     // then the main thread will re-initiate a fuse if there is a waiting request.
+  //   }
+  // }
+
+  // while (!m_stop.load()) {
+
+  //   std::unique_lock<std::mutex> lock(m_mutex);
+  //   std::cout << "THREAD " << whichThread << " IN LOOP, WAITING" << std::endl;
+  //   m_conditionVar.wait(lock, [this] { return !m_fuseRequests.empty() || m_stop.load(); });
+  //   std::vector<glm::vec3> data = m_fuseRequests.front();
+  //   m_fuseRequests.pop();
+  //   lock.unlock();
+
+  //   std::cout << "THREAD " << whichThread << " SIGNALLED, RUNNING" << std::endl;
+  //   doFuse(whichThread, nThreads, data);
+  // }
   std::cout << "THREAD " << whichThread << " EXITING" << std::endl;
 }
 
@@ -404,7 +99,19 @@ Fuse::fuse(const std::vector<glm::vec3>& colorsPerChannel)
   // if threads are working, stash this request.
   // notify threads with colorsPerChannel info
   if (FUSE_THREADED) {
-    // signal the threads
+    std::lock_guard<std::mutex> lock(m_mutex);
+    // for interactivity, we only want at most one item on the queue(?)
+    if (m_fuseRequests.full()) {
+      // replace existing...
+      std::vector<glm::vec3> old;
+      m_fuseRequests.pop(old);
+      m_fuseRequests.push(colorsPerChannel);
+    } else if (m_fuseRequests.empty()) {
+      m_fuseRequests.push(colorsPerChannel);
+    } else {
+      // some kind of error, we don't want to get here.
+    }
+    // signal the threads w/new data
     m_conditionVar.notify_all();
   } else {
     // just run as single thread
--- renderlib/Fuse.h
+++ renderlib/Fuse.h
@@ -4,9 +4,338 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <future>
+#include <queue>
 #include <thread>
 #include <type_traits>
 #include <vector>
+
+template<typename T>
+class unbounded_queue
+{
+public:
+  explicit unbounded_queue(bool block = true)
+    : m_block{ block }
+  {}
+
+  void push(const T& item)
+  {
+    {
+      std::scoped_lock guard(m_queue_lock);
+      m_queue.push(item);
+    }
+    m_condition.notify_one();
+  }
+
+  void push(T&& item)
+  {
+    {
+      std::scoped_lock guard(m_queue_lock);
+      m_queue.push(std::move(item));
+    }
+    m_condition.notify_one();
+  }
+
+  template<typename... Args>
+  void emplace(Args&&... args)
+  {
+    {
+      std::scoped_lock guard(m_queue_lock);
+      m_queue.emplace(std::forward<Args>(args)...);
+    }
+    m_condition.notify_one();
+  }
+
+  bool try_push(const T& item)
+  {
+    {
+      std::unique_lock lock(m_queue_lock, std::try_to_lock);
+      if (!lock)
+        return false;
+      m_queue.push(item);
+    }
+    m_condition.notify_one();
+    return true;
+  }
+
+  bool try_push(T&& item)
+  {
+    {
+      std::unique_lock lock(m_queue_lock, std::try_to_lock);
+      if (!lock)
+        return false;
+      m_queue.push(std::move(item));
+    }
+    m_condition.notify_one();
+    return true;
+  }
+
+  bool pop(T& item)
+  {
+    std::unique_lock guard(m_queue_lock);
+    m_condition.wait(guard, [&]() { return !m_queue.empty() || !m_block; });
+    if (m_queue.empty())
+      return false;
+    item = std::move(m_queue.front());
+    m_queue.pop();
+    return true;
+  }
+
+  bool try_pop(T& item)
+  {
+    std::unique_lock lock(m_queue_lock, std::try_to_lock);
+    if (!lock || m_queue.empty())
+      return false;
+    item = std::move(m_queue.front());
+    m_queue.pop();
+    return true;
+  }
+
+  std::size_t size() const
+  {
+    std::scoped_lock guard(m_queue_lock);
+    return m_queue.size();
+  }
+
+  bool empty() const
+  {
+    std::scoped_lock guard(m_queue_lock);
+    return m_queue.empty();
+  }
+
+  void block()
+  {
+    std::scoped_lock guard(m_queue_lock);
+    m_block = true;
+  }
+
+  void unblock()
+  {
+    {
+      std::scoped_lock guard(m_queue_lock);
+      m_block = false;
+    }
+    m_condition.notify_all();
+  }
+
+  bool blocking() const
+  {
+    std::scoped_lock guard(m_queue_lock);
+    return m_block;
+  }
+
+private:
+  using queue_t = std::queue<T>;
+  queue_t m_queue;
+
+  bool m_block;
+
+  mutable std::mutex m_queue_lock;
+  std::condition_variable m_condition;
+};
+
+template<typename T>
+class bounded_queue
+{
+public:
+  explicit bounded_queue(std::size_t max_size, bool block = true)
+    : m_block{ block }
+    , m_max_size{ max_size }
+  {
+    if (!m_max_size)
+      throw std::invalid_argument("bad queue max-size! must be non-zero!");
+  }
+
+  bool push(const T& item)
+  {
+    {
+      std::unique_lock guard(m_queue_lock);
+      m_condition_push.wait(guard, [&]() { return m_queue.size() < m_max_size || !m_block; });
+      if (m_queue.size() == m_max_size)
+        return false;
+      m_queue.push(item);
+    }
+    m_condition_pop.notify_one();
+    return true;
+  }
+
+  bool push(T&& item)
+  {
+    {
+      std::unique_lock guard(m_queue_lock);
+      m_condition_push.wait(guard, [&]() { return m_queue.size() < m_max_size || !m_block; });
+      if (m_queue.size() == m_max_size)
+        return false;
+      m_queue.push(std::move(item));
+    }
+    m_condition_pop.notify_one();
+    return true;
+  }
+
+  template<typename... Args>
+  bool emplace(Args&&... args)
+  {
+    {
+      std::unique_lock guard(m_queue_lock);
+      m_condition_push.wait(guard, [&]() { return m_queue.size() < m_max_size || !m_block; });
+      if (m_queue.size() == m_max_size)
+        return false;
+      m_queue.emplace(std::forward<Args>(args)...);
+    }
+    m_condition_pop.notify_one();
+    return true;
+  }
+
+  bool pop(T& item)
+  {
+    {
+      std::unique_lock guard(m_queue_lock);
+      m_condition_pop.wait(guard, [&]() { return !m_queue.empty() || !m_block; });
+      if (m_queue.empty())
+        return false;
+      item = std::move(m_queue.front());
+      m_queue.pop();
+    }
+    m_condition_push.notify_one();
+    return true;
+  }
+
+  std::size_t size() const
+  {
+    std::scoped_lock guard(m_queue_lock);
+    return m_queue.size();
+  }
+
+  std::size_t capacity() const { return m_max_size; }
+
+  bool empty() const
+  {
+    std::scoped_lock guard(m_queue_lock);
+    return m_queue.empty();
+  }
+
+  bool full() const
+  {
+    std::scoped_lock lock(m_queue_lock);
+    return m_queue.size() == capacity();
+  }
+
+  void block()
+  {
+    std::scoped_lock guard(m_queue_lock);
+    m_block = true;
+  }
+
+  void unblock()
+  {
+    {
+      std::scoped_lock guard(m_queue_lock);
+      m_block = false;
+    }
+    m_condition_push.notify_all();
+    m_condition_pop.notify_all();
+  }
+
+  bool blocking() const
+  {
+    std::scoped_lock guard(m_queue_lock);
+    return m_block;
+  }
+
+private:
+  using queue_t = std::queue<T>;
+  queue_t m_queue;
+
+  bool m_block;
+  const std::size_t m_max_size;
+
+  mutable std::mutex m_queue_lock;
+  std::condition_variable m_condition_push;
+  std::condition_variable m_condition_pop;
+};
+
+class thread_pool
+{
+public:
+  explicit thread_pool(std::size_t thread_count = std::thread::hardware_concurrency())
+    : m_queues(thread_count)
+    , m_count(thread_count)
+  {
+    if (!thread_count)
+      throw std::invalid_argument("bad thread count! must be non-zero!");
+
+    auto worker = [this](auto i) {
+      while (true) {
+        proc_t f;
+        for (auto n = 0; n < m_count * K; ++n)
+          if (m_queues[(i + n) % m_count].try_pop(f))
+            break;
+        if (!f && !m_queues[i].pop(f))
+          break;
+        f();
+      }
+    };
+
+    m_threads.reserve(thread_count);
+    for (auto i = 0; i < thread_count; ++i)
+      m_threads.emplace_back(worker, i);
+  }
+
+  ~thread_pool()
+  {
+    for (auto& queue : m_queues)
+      queue.unblock();
+    for (auto& thread : m_threads)
+      thread.join();
+  }
+
+  template<typename F, typename... Args>
+  void enqueue_work(F&& f, Args&&... args)
+  {
+    auto work = [p = std::forward<F>(f), t = std::make_tuple(std::forward<Args>(args)...)]() { std::apply(p, t); };
+    auto i = m_index++;
+
+    for (auto n = 0; n < m_count * K; ++n)
+      if (m_queues[(i + n) % m_count].try_push(work))
+        return;
+
+    m_queues[i % m_count].push(std::move(work));
+  }
+
+  template<typename F, typename... Args>
+  [[nodiscard]] auto enqueue_task(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>>
+  {
+    using task_return_type = std::invoke_result_t<F, Args...>;
+    using task_type = std::packaged_task<task_return_type()>;
+
+    auto task = std::make_shared<task_type>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+    auto work = [=]() { (*task)(); };
+    auto result = task->get_future();
+    auto i = m_index++;
+
+    for (auto n = 0; n < m_count * K; ++n)
+      if (m_queues[(i + n) % m_count].try_push(work))
+        return result;
+
+    m_queues[i % m_count].push(std::move(work));
+
+    return result;
+  }
+
+private:
+  using proc_t = std::function<void(void)>;
+  using queue_t = unbounded_queue<proc_t>;
+  using queues_t = std::vector<queue_t>;
+  queues_t m_queues;
+
+  using threads_t = std::vector<std::thread>;
+  threads_t m_threads;
+
+  const std::size_t m_count;
+  std::atomic_uint m_index = 0;
+
+  inline static const unsigned int K = 2;
+};
 
 class ImageXYZC;
 
@@ -32,12 +361,18 @@ private:
 
   std::condition_variable m_conditionVar;
   std::mutex m_mutex;
-  bool m_stop = false;
+  std::atomic_bool m_stop = false;
 
   // the read-only volume data
   const ImageXYZC* m_img;
   // the fused result (always rewrite to same mem location)
   uint8_t* m_outRGBVolume;
+
+  // double-buffered so that one can be waiting while the other processing
+  bounded_queue<std::vector<glm::vec3>> m_fuseRequests;
+  //  std::queue<std::vector<glm::vec3>> m_fuseRequests;
+  // std::vector<glm::vec3> m_colorsToFuse;
+  // std::vector<glm::vec3> m_queuedColorsToFuse;
 
   void fuseThreadWorker(size_t whichThread, size_t nThreads);
   void doFuse(size_t whichThread, size_t nThreads, const std::vector<glm::vec3>& colors);
