3f3bf6bde6ef7113cbe470763b5c828f2a05c635
toloudis <danielt@alleninstitute.org>
2022-01-07 13:01:28 -0800

    Bump to 1.2.2

--- agave_pyclient/setup.py
+++ agave_pyclient/setup.py
@@ -84,6 +84,6 @@ setup(
     url="https://github.com/allen-cell-animated/agave",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.2.1",
+    version="1.2.2",
     zip_safe=False,
 )
