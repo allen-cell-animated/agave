ad454a7dc64fa5646cd35d08b3dfcce84ab9ab4d
dmt <dmt@completelydifferent.com>
2022-01-30 20:28:13 -0800

    fix trim?

--- renderlib/FileReaderTIFF.cpp
+++ renderlib/FileReaderTIFF.cpp
@@ -29,7 +29,7 @@ trim(const std::string& str, const std::string& whitespace = " \t\r\n")
     return ""; // no content
 
   const auto strEnd = str.find_last_not_of(whitespace);
-  const auto strRange = strEnd - strBegin;
+  const auto strRange = strEnd - strBegin + 1;
 
   return str.substr(strBegin, strRange);
 }
