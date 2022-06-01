ae19bd0cced6d5bce273ed44735f961ecb0d1c9b
toloudis <danielt@alleninstitute.org>
2022-06-01 12:43:40 -0700

    add a parallel for loop implementation and use it in a couple of places

--- renderlib/ImageXyzcGpu.cpp
+++ renderlib/ImageXyzcGpu.cpp
@@ -2,7 +2,7 @@
 
 #include "ImageXYZC.h"
 #include "Logging.h"
-#include "gl/Util.h"
+#include "threading.h"
 
 #include "gl/Util.h"
 
@@ -116,11 +116,19 @@ ImageGpu::updateVolumeData4x16(ImageXYZC* img, int c0, int c1, int c2, int c3)
   size_t xyz = img->sizeX() * img->sizeY() * img->sizeZ();
   uint16_t* v = new uint16_t[xyz * N];
 
-  for (uint32_t i = 0; i < xyz; ++i) {
-    for (int j = 0; j < N; ++j) {
-      v[N * (i) + j] = img->channel(ch[j])->m_ptr[(i)];
+  // for (uint32_t i = 0; i < xyz; ++i) {
+  //   for (int j = 0; j < N; ++j) {
+  //     v[N * (i) + j] = img->channel(ch[j])->m_ptr[(i)];
+  //   }
+  // }
+
+  parallel_for(xyz, [&v, &img, &ch](size_t s, size_t e) {
+    for (size_t i = s; i < e; ++i) {
+      for (int j = 0; j < N; ++j) {
+        v[N * (i) + j] = img->channel(ch[j])->m_ptr[(i)];
+      }
     }
-  }
+  });
 
   auto endTime = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed = endTime - startTime;
