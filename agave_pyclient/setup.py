b988e3ecb3ba811fbe66f3dc34b56464111d104a
toloudis <danielt@alleninstitute.org>
2022-07-05 16:36:09 -0700

    Bump to 1.4.1

--- agave_pyclient/setup.py
+++ agave_pyclient/setup.py
@@ -84,6 +84,6 @@ setup(
     url="https://github.com/allen-cell-animated/agave",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.4.0",
+    version="1.4.1",
     zip_safe=False,
 )
--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Allen Institute for Cell Science"
 author = "Allen Institute for Cell Science"
 
 # The full version, including alpha/beta/rc tags
-release = "1.4.0"
+release = "1.4.1"
 
 
 # -- General configuration ---------------------------------------------------
