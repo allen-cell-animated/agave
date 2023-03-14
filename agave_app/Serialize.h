e8b814647f79cf9de3f2e00344d9b8e8c45a1261
toloudis <danielt@alleninstitute.org>
2023-03-14 12:26:02 -0700

    add defaults and equality operators

--- agave_app/Serialize.h
+++ agave_app/Serialize.h
@@ -8,28 +8,49 @@
 
 struct PathTraceSettings_V1
 {
-  float primaryStepSize;
-  float secondaryStepSize;
+  float primaryStepSize = 4.0f;
+  float secondaryStepSize = 4.0f;
+  bool operator==(const PathTraceSettings_V1& other) const
+  {
+    return primaryStepSize == other.primaryStepSize && secondaryStepSize == other.secondaryStepSize;
+  }
   NLOHMANN_DEFINE_TYPE_INTRUSIVE(PathTraceSettings_V1, primaryStepSize, secondaryStepSize)
 };
 struct TimelineSettings_V1
 {
-  float minTime;
-  float maxTime;
-  float currentTime;
+  float minTime = 0;
+  float maxTime = 0;
+  float currentTime = 0;
+  bool operator==(const TimelineSettings_V1& other) const
+  {
+    return minTime == other.minTime && maxTime == other.maxTime && currentTime == other.currentTime;
+  }
   NLOHMANN_DEFINE_TYPE_INTRUSIVE(TimelineSettings_V1, minTime, maxTime, currentTime)
 };
+
+enum class Projection : int
+{
+  PERSPECTIVE = 0,
+  ORTHOGRAPHIC = 1
+};
+
 struct CameraSettings_V1
 {
-  std::array<float, 3> eye;
-  std::array<float, 3> target;
-  std::array<float, 3> up;
-  int projection;
-  float fovY;
-  float orthoScale;
-  float exposure;
-  float aperture;
-  float focalDistance;
+  std::array<float, 3> eye = { 0, 0, -1 };
+  std::array<float, 3> target = { 0, 0, 0 };
+  std::array<float, 3> up = { 0, 1, 0 };
+  Projection projection = Projection::PERSPECTIVE;
+  float fovY = 55.0f;
+  float orthoScale = 1.0f;
+  float exposure = 0.75f;
+  float aperture = 0.0f;
+  float focalDistance = 1.0f;
+  bool operator==(const CameraSettings_V1& other) const
+  {
+    return eye == other.eye && target == other.target && up == other.up && projection == other.projection &&
+           fovY == other.fovY && orthoScale == other.orthoScale && exposure == other.exposure &&
+           aperture == other.aperture && focalDistance == other.focalDistance;
+  }
   NLOHMANN_DEFINE_TYPE_INTRUSIVE(CameraSettings_V1,
                                  eye,
                                  target,
@@ -43,33 +64,56 @@ struct CameraSettings_V1
 };
 struct ControlPointSettings_V1
 {
-  float x;
-  std::array<float, 4> value;
+  float x = 0.0f;
+  std::array<float, 4> value = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+  bool operator==(const ControlPointSettings_V1& other) const { return x == other.x && value == other.value; }
   NLOHMANN_DEFINE_TYPE_INTRUSIVE(ControlPointSettings_V1, x, value)
 };
+
+enum class GradientEditMode : int
+{
+  WINDOW_LEVEL = 0,
+  ISOVALUE = 1,
+  PERCENTILE = 2,
+  CUSTOM = 3
+};
+
 struct LutParams_V1
 {
-  float window;
-  float level;
-  float isovalue;
-  float isorange;
-  float pctLow;
-  float pctHigh;
+  float window = 0.5f;
+  float level = 0.5f;
+  float isovalue = 0.5f;
+  float isorange = 0.01f;
+  float pctLow = 0.5f;
+  float pctHigh = 0.98f;
   std::vector<ControlPointSettings_V1> controlPoints;
-  int mode;
+  GradientEditMode mode = GradientEditMode::WINDOW_LEVEL;
 
+  bool operator==(const LutParams_V1& other) const
+  {
+    return window == other.window && level == other.level && isovalue == other.isovalue && isorange == other.isorange &&
+           pctLow == other.pctLow && pctHigh == other.pctHigh && controlPoints == other.controlPoints &&
+           mode == other.mode;
+  }
   NLOHMANN_DEFINE_TYPE_INTRUSIVE(LutParams_V1, window, level, isovalue, isorange, pctLow, pctHigh, controlPoints, mode)
 };
 struct ChannelSettings_V1
 {
-  bool enabled;
-  std::array<float, 3> diffuseColor;
-  std::array<float, 3> specularColor;
-  std::array<float, 3> emissiveColor;
-  float glossiness;
-  float opacity;
+  bool enabled = true;
+  std::array<float, 3> diffuseColor = { 1.0f, 1.0f, 1.0f };
+  std::array<float, 3> specularColor = { 0.0f, 0.0f, 0.0f };
+  std::array<float, 3> emissiveColor = { 0.0f, 0.0f, 0.0f };
+  float glossiness = 0.0f;
+  float opacity = 1.0f;
   LutParams_V1 lutParams;
 
+  bool operator==(const ChannelSettings_V1& other) const
+  {
+    return enabled == other.enabled && diffuseColor == other.diffuseColor && specularColor == other.specularColor &&
+           emissiveColor == other.emissiveColor && glossiness == other.glossiness && opacity == other.opacity &&
+           lutParams == other.lutParams;
+  }
   NLOHMANN_DEFINE_TYPE_INTRUSIVE(ChannelSettings_V1,
                                  enabled,
                                  diffuseColor,
@@ -80,23 +124,38 @@ struct ChannelSettings_V1
                                  lutParams)
 };
 
-struct LightSettings
+enum class LightType : int
 {
-  int type;
-  float distance;
-  float theta;
-  float phi;
-  std::array<float, 3> color;
-  float colorIntensity;
-  std::array<float, 3> topColor;
-  float topColorIntensity;
-  std::array<float, 3> middleColor;
-  float middleColorIntensity;
-  std::array<float, 3> bottomColor;
-  float bottomColorIntensity;
-  float width;
-  float height;
-  NLOHMANN_DEFINE_TYPE_INTRUSIVE(LightSettings,
+  SKY = 0,
+  AREA = 1
+};
+
+struct LightSettings_V1
+{
+  LightType type = LightType::SKY;
+  float distance = 1.0f;
+  float theta = 0.0f;
+  float phi = 3.14159265358979323846f / 2.0f;
+  std::array<float, 3> color = { 1.0f, 1.0f, 1.0f };
+  float colorIntensity = 1.0f;
+  std::array<float, 3> topColor = { 1.0f, 1.0f, 1.0f };
+  float topColorIntensity = 1.0f;
+  std::array<float, 3> middleColor = { 1.0f, 1.0f, 1.0f };
+  float middleColorIntensity = 1.0f;
+  std::array<float, 3> bottomColor = { 1.0f, 1.0f, 1.0f };
+  float bottomColorIntensity = 1.0f;
+  float width = 1.0f;
+  float height = 1.0f;
+
+  bool operator==(const LightSettings_V1& other) const
+  {
+    return type == other.type && distance == other.distance && theta == other.theta && phi == other.phi &&
+           color == other.color && colorIntensity == other.colorIntensity && topColor == other.topColor &&
+           topColorIntensity == other.topColorIntensity && middleColor == other.middleColor &&
+           middleColorIntensity == other.middleColorIntensity && bottomColor == other.bottomColor &&
+           bottomColorIntensity == other.bottomColorIntensity && width == other.width && height == other.height;
+  }
+  NLOHMANN_DEFINE_TYPE_INTRUSIVE(LightSettings_V1,
                                  type,
                                  distance,
                                  theta,
@@ -113,17 +172,30 @@ struct LightSettings
                                  height)
 };
 
+enum class DurationType : int
+{
+  SAMPLES = 0,
+  TIME = 1
+};
+
 struct CaptureSettings_V1
 {
-  int width;
-  int height;
-  std::string filenamePrefix;
-  std::string outputDirectory;
-  int samples;
-  float seconds;
-  int durationType;
-  int startTime;
-  int endTime;
+  int width = 960;
+  int height = 540;
+  std::string filenamePrefix = "frame";
+  std::string outputDirectory = ".";
+  int samples = 32;
+  float seconds = 10.0f;
+  DurationType durationType = DurationType::SAMPLES;
+  int startTime = 0;
+  int endTime = 0;
+
+  bool operator==(const CaptureSettings_V1& other) const
+  {
+    return width == other.width && height == other.height && filenamePrefix == other.filenamePrefix &&
+           outputDirectory == other.outputDirectory && samples == other.samples && seconds == other.seconds &&
+           durationType == other.durationType && startTime == other.startTime && endTime == other.endTime;
+  }
   NLOHMANN_DEFINE_TYPE_INTRUSIVE(CaptureSettings_V1,
                                  width,
                                  height,
@@ -141,36 +213,45 @@ struct ViewerState_V1
   std::string name; // m_volumeImageFile
   // the version of this schema
   // use app version
-  std::array<uint32_t, 3> version;
-  std::array<int, 2> resolution; // m_resolutionX, m_resolutionY
-  int renderIterations;          // m_renderIterations
+  std::array<uint32_t, 3> version{ 0, 0, 0 };
+  std::array<int, 2> resolution = { 0, 0 }; // m_resolutionX, m_resolutionY
+  int renderIterations = 1;                 // m_renderIterations
 
   PathTraceSettings_V1 pathTracer; // m_primaryStepSize, m_secondaryStepSize
   TimelineSettings_V1 timeline;    // m_minTime, m_maxTime, m_currentTime
-  int scene;                       // m_currentScene
+  int scene = 0;                   // m_currentScene
 
   // [[xm, xM], [ym, yM], [zm, zM]]
-  std::array<std::array<float, 2>, 3> clipRegion;
+  std::array<std::array<float, 2>, 3> clipRegion = { 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f };
 
-  std::array<float, 3> scale; // m_scaleX, m_scaleY, m_scaleZ
+  std::array<float, 3> scale = { 1, 1, 1 }; // m_scaleX, m_scaleY, m_scaleZ
 
   CameraSettings_V1 camera;
 
-  std::array<float, 3> backgroundColor; // m_backgroundColor
+  std::array<float, 3> backgroundColor = { 0, 0, 0 }; // m_backgroundColor
 
-  std::array<float, 3> boundingBoxColor; // m_boundingBoxColor
+  std::array<float, 3> boundingBoxColor = { 1, 1, 1 }; // m_boundingBoxColor
 
-  bool showBoundingBox; // m_showBoundingBox
+  bool showBoundingBox = false; // m_showBoundingBox
 
   std::vector<ChannelSettings_V1> channels; // m_channels
 
-  float density;
+  float density = 50.0f;
 
   // lighting
-  std::vector<LightSettings> lights; // m_lights
+  std::vector<LightSettings_V1> lights; // m_lights
 
   CaptureSettings_V1 capture;
 
+  bool operator==(const ViewerState_V1& other) const
+  {
+    return name == other.name && version == other.version && resolution == other.resolution &&
+           renderIterations == other.renderIterations && pathTracer == other.pathTracer && timeline == other.timeline &&
+           scene == other.scene && clipRegion == other.clipRegion && scale == other.scale && camera == other.camera &&
+           backgroundColor == other.backgroundColor && boundingBoxColor == other.boundingBoxColor &&
+           showBoundingBox == other.showBoundingBox && channels == other.channels && density == other.density &&
+           lights == other.lights && capture == other.capture;
+  }
   NLOHMANN_DEFINE_TYPE_INTRUSIVE(ViewerState_V1,
                                  name,
                                  version,
--- test/test_serialize.cpp
+++ test/test_serialize.cpp
@@ -17,8 +17,7 @@ TEST_CASE("Json Serialization", "[serialize]")
     std::string str = json.dump();
     json = nlohmann::json::parse(str);
     auto settings2 = json.get<PathTraceSettings_V1>();
-    REQUIRE(settings.primaryStepSize == settings2.primaryStepSize);
-    REQUIRE(settings.secondaryStepSize == settings2.secondaryStepSize);
+    REQUIRE(settings == settings2);
   }
   SECTION("Read and write TimelineSettings_V1")
   {
@@ -30,9 +29,7 @@ TEST_CASE("Json Serialization", "[serialize]")
     std::string str = json.dump();
     json = nlohmann::json::parse(str);
     auto settings2 = json.get<TimelineSettings_V1>();
-    REQUIRE(settings.minTime == settings2.minTime);
-    REQUIRE(settings.maxTime == settings2.maxTime);
-    REQUIRE(settings.currentTime == settings2.currentTime);
+    REQUIRE(settings == settings2);
   }
   SECTION("Read and write CameraSettings_V1")
   {
@@ -40,7 +37,7 @@ TEST_CASE("Json Serialization", "[serialize]")
     settings.eye = { 0.1f, 0.2f, 0.3f };
     settings.target = { 0.4f, 0.5f, 0.6f };
     settings.up = { 0.7f, 0.8f, 0.9f };
-    settings.projection = 1;
+    settings.projection = Projection::PERSPECTIVE;
     settings.fovY = 0.1f;
     settings.orthoScale = 0.2f;
     settings.exposure = 0.3f;
@@ -50,15 +47,7 @@ TEST_CASE("Json Serialization", "[serialize]")
     std::string str = json.dump();
     json = nlohmann::json::parse(str);
     auto settings2 = json.get<CameraSettings_V1>();
-    REQUIRE(settings.eye == settings2.eye);
-    REQUIRE(settings.target == settings2.target);
-    REQUIRE(settings.up == settings2.up);
-    REQUIRE(settings.projection == settings2.projection);
-    REQUIRE(settings.fovY == settings2.fovY);
-    REQUIRE(settings.orthoScale == settings2.orthoScale);
-    REQUIRE(settings.exposure == settings2.exposure);
-    REQUIRE(settings.aperture == settings2.aperture);
-    REQUIRE(settings.focalDistance == settings2.focalDistance);
+    REQUIRE(settings == settings2);
   }
 
   SECTION("Read and write ControlPointSettings_V1")
@@ -70,8 +59,7 @@ TEST_CASE("Json Serialization", "[serialize]")
     std::string str = json.dump();
     json = nlohmann::json::parse(str);
     auto settings2 = json.get<ControlPointSettings_V1>();
-    REQUIRE(settings.x == settings2.x);
-    REQUIRE(settings.value == settings2.value);
+    REQUIRE(settings == settings2);
   }
 
   SECTION("Read and write LutParams_V1")
@@ -84,19 +72,12 @@ TEST_CASE("Json Serialization", "[serialize]")
     settings.pctLow = 0.5f;
     settings.pctHigh = 0.6f;
     settings.controlPoints = { { 0.1f, { 0.2f, 0.3f, 0.4f, 0.5f } }, { 0.2f, { 0.3f, 0.4f, 0.5f, 0.6f } } };
-    settings.mode = 1;
+    settings.mode = GradientEditMode::CUSTOM;
     nlohmann::json json = settings;
     std::string str = json.dump();
     json = nlohmann::json::parse(str);
     auto settings2 = json.get<LutParams_V1>();
-    REQUIRE(settings.window == settings2.window);
-    REQUIRE(settings.level == settings2.level);
-    REQUIRE(settings.isovalue == settings2.isovalue);
-    REQUIRE(settings.isorange == settings2.isorange);
-    REQUIRE(settings.pctLow == settings2.pctLow);
-    REQUIRE(settings.pctHigh == settings2.pctHigh);
-    // REQUIRE(settings.controlPoints == settings2.controlPoints);
-    REQUIRE(settings.mode == settings2.mode);
+    REQUIRE(settings == settings2);
   }
 
   SECTION("Read and write ChannelSettings_V1")
@@ -110,19 +91,83 @@ TEST_CASE("Json Serialization", "[serialize]")
                            0.5f,
                            0.6f,
                            { { 0.1f, { 0.2f, 0.3f, 0.4f, 0.5f } }, { 0.2f, { 0.3f, 0.4f, 0.5f, 0.6f } } },
-                           1 };
+                           GradientEditMode::CUSTOM };
     nlohmann::json json = settings;
     std::string str = json.dump();
     json = nlohmann::json::parse(str);
     auto settings2 = json.get<ChannelSettings_V1>();
-    REQUIRE(settings.enabled == settings2.enabled);
-    REQUIRE(settings.lutParams.window == settings2.lutParams.window);
-    REQUIRE(settings.lutParams.level == settings2.lutParams.level);
-    REQUIRE(settings.lutParams.isovalue == settings2.lutParams.isovalue);
-    REQUIRE(settings.lutParams.isorange == settings2.lutParams.isorange);
-    REQUIRE(settings.lutParams.pctLow == settings2.lutParams.pctLow);
-    REQUIRE(settings.lutParams.pctHigh == settings2.lutParams.pctHigh);
-    // REQUIRE(settings.lutParams.controlPoints == settings2.lutParams.controlPoints);
-    REQUIRE(settings.lutParams.mode == settings2.lutParams.mode);
+    REQUIRE(settings == settings2);
+  }
+
+  SECTION("Read and write LightSettings_V1")
+  {
+    auto settings = LightSettings_V1{};
+    settings.type = LightType::AREA;
+    settings.distance = 0.1;
+    settings.theta = 0.2;
+    settings.phi = 0.3;
+    settings.color = { 0.1f, 0.2f, 0.3f };
+    settings.colorIntensity = 0.4f;
+    settings.topColor = { 0.5f, 0.6f, 0.7f };
+    settings.topColorIntensity = 0.8f;
+    settings.middleColor = { 0.9f, 0.1f, 0.2f };
+    settings.middleColorIntensity = 0.3f;
+    settings.bottomColor = { 0.9f, 0.1f, 0.2f };
+    settings.bottomColorIntensity = 0.3f;
+    settings.width = 0.4f;
+    settings.height = 0.5f;
+    nlohmann::json json = settings;
+    std::string str = json.dump();
+    json = nlohmann::json::parse(str);
+    auto settings2 = json.get<LightSettings_V1>();
+    REQUIRE(settings == settings2);
+  }
+
+  SECTION("Read and write CaptureSettings_V1")
+  {
+    auto settings = CaptureSettings_V1{};
+    settings.width = 1;
+    settings.height = 2;
+    settings.filenamePrefix = "test";
+    settings.outputDirectory = "test2";
+    settings.samples = 3;
+    settings.seconds = 4.0;
+    settings.durationType = DurationType::TIME;
+    settings.startTime = 6;
+    settings.endTime = 7;
+    nlohmann::json json = settings;
+    std::string str = json.dump();
+    json = nlohmann::json::parse(str);
+    auto settings2 = json.get<CaptureSettings_V1>();
+    REQUIRE(settings == settings2);
+  }
+
+  SECTION("Read and write ViewerState_V1")
+  {
+    auto settings = ViewerState_V1{};
+    settings.name = "test";
+    settings.version = { 1, 2, 3 };
+    settings.resolution = { 1, 2 };
+    settings.renderIterations = 3;
+    settings.pathTracer = PathTraceSettings_V1{
+      0.1f,
+      0.2f,
+    };
+    settings.timeline = TimelineSettings_V1{ 0.1, 0.2, 0.3 };
+    settings.scene = 1;
+    settings.clipRegion = { 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f };
+    settings.scale = { 0.1f, 0.2f, 0.3f };
+    settings.camera = CameraSettings_V1{};
+    settings.backgroundColor = { 0.1f, 0.2f, 0.3f };
+    settings.showBoundingBox = true;
+    settings.channels = { ChannelSettings_V1{}, ChannelSettings_V1{} };
+    settings.density = 100.0;
+    settings.lights = { LightSettings_V1{}, LightSettings_V1{} };
+    settings.capture = CaptureSettings_V1{};
+    nlohmann::json json = settings;
+    std::string str = json.dump();
+    json = nlohmann::json::parse(str);
+    auto settings2 = json.get<ViewerState_V1>();
+    REQUIRE(settings == settings2);
   }
 }
