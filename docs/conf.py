56a31e009ea2a46e5bb643fff15a3946805eb56d
toloudis <danielt@alleninstitute.org>
2023-05-05 09:54:47 -0700

    Bump to 1.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Allen Institute for Cell Science"
 author = "Allen Institute for Cell Science"
 
 # The full version, including alpha/beta/rc tags
-release = "1.5.0-rc.3"
+release = "1.5.0"
 
 
 # -- General configuration ---------------------------------------------------
