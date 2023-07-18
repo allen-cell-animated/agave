6c35adf91599d09ebdc556fe57fc75f060fd7972
toloudis <danielt@alleninstitute.org>
2023-07-18 13:27:10 -0700

    make test work

---
+++ webclient/src/test/placeholder.test.ts
@@ -0,0 +1,7 @@
+import { describe, it } from "mocha";
+import { assert } from "chai";
+describe("Placeholder", function () {
+  it("passes always", function () {
+    assert.equal(true, true);
+  });
+});
