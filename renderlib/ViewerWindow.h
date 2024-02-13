f52cd3c1a91afb0f349d0fa5d2fae2767b1f88c2
toloudis <danielt@alleninstitute.org>
2024-02-13 09:43:27 -0800

    fix bug with selection buffer not being cleared

--- renderlib/ViewerWindow.h
+++ renderlib/ViewerWindow.h
@@ -34,6 +34,13 @@ public:
     }
     m_activeTool = (tool ? tool : &m_defaultTool);
 
+    // clear out the buffer once.
+    // we could alternatively flag this for clearing on the next update.
+    // see in update() where we check for no vertices.
+    if (!tool) {
+      m_selection.clear();
+    }
+
     // Todo: this could be replaced with a push/pop mechanism to allow
     //       the completion of a tool to restore a previous state.
   }
@@ -43,8 +50,9 @@ public:
   template<typename Fn>
   void forEachTool(Fn fn)
   {
-    for (ManipulationTool* tool : m_tools)
+    for (ManipulationTool* tool : m_tools) {
       fn(tool);
+    }
     fn(m_activeTool);
   }
 
