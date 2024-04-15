1c3a023fc6df11772b19b07d603fc92ac6396a1d
Dan Toloudis <danielt@alleninstitute.org>
2024-04-15 15:55:06 -0700

    Bump to 1.6.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Allen Institute for Cell Science"
 author = "Allen Institute for Cell Science"
 
 # The full version, including alpha/beta/rc tags
-release = "1.6.1"
+release = "1.6.2"
 
 
 # -- General configuration ---------------------------------------------------
