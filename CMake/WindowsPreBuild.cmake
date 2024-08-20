4591fd354fbc323dbde3df990e4cbf55c60182ec
Dan Toloudis <danielt@alleninstitute.org>
2024-08-20 16:34:31 -0700

    correctly remove bin, lib, include from windows installer

--- CMake/WindowsPreBuild.cmake
+++ CMake/WindowsPreBuild.cmake
@@ -3,6 +3,9 @@ if(NOT DEFINED APP_DIR)
 endif()
 
 # remove unneeded dirs from installer
-execute_process(COMMAND rd /s /q "${APP_DIR}\\bin")
-execute_process(COMMAND rd /s /q "${APP_DIR}\\include")
-execute_process(COMMAND rd /s /q "${APP_DIR}\\lib")
+file(REMOVE_RECURSE "${APP_DIR}/bin")
+file(REMOVE_RECURSE "${APP_DIR}/include")
+file(REMOVE_RECURSE "${APP_DIR}/lib")
+#execute_process(COMMAND rd /s /q "${APP_DIR}\\bin")
+#execute_process(COMMAND rd /s /q "${APP_DIR}\\include")
+#execute_process(COMMAND rd /s /q "${APP_DIR}\\lib")
