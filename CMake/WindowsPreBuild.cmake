405e5022fe77b1cad2b27d7b7cab4a8eab25efba
dmt <dmt@completelydifferent.com>
2024-08-19 18:55:54 -0700

    tweaking for windows

--- CMake/WindowsPreBuild.cmake
+++ CMake/WindowsPreBuild.cmake
@@ -1,3 +1,8 @@
-# remove unneeded dirs from installer 
+if(NOT DEFINED APP_DIR)
+  set(APP_DIR "${CPACK_TEMPORARY_INSTALL_DIRECTORY}")
+endif()
+
+# remove unneeded dirs from installer
+execute_process(COMMAND rd -r "${APP_DIR}/bin")
 execute_process(COMMAND rd -r "${APP_DIR}/include")
 execute_process(COMMAND rd -r "${APP_DIR}/lib")
