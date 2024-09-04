8e2747d89aa77c9d749b67e5500b0a00bed26387
dmt <dmt@completelydifferent.com>
2024-09-03 20:17:39 -0700

    some init for wgpu renderer

--- renderlib_wgpu/RenderWgpuPT.cpp
+++ renderlib_wgpu/RenderWgpuPT.cpp
@@ -1,6 +1,15 @@
 #include "RenderWgpuPT.h"
 
-RenderWgpuPT::RenderWgpuPT(RenderSettings* rs) {}
+#include "../renderlib/Status.h"
+
+RenderWgpuPT::RenderWgpuPT(RenderSettings* rs)
+  : m_scene(nullptr)
+  , m_w(0)
+  , m_h(0)
+  , m_renderSettings(rs)
+  , m_status(new CStatus)
+{
+}
 RenderWgpuPT::~RenderWgpuPT() {}
 
 void
