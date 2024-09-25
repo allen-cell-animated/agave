7d4e83a0a4cc2dd947fb60f6b618bb00f05cd580
toloudis <danielt@alleninstitute.org>
2024-09-24 18:09:16 -0700

    Bump to 1.7.1

--- agave_pyclient/agave_pyclient/__init__.py
+++ agave_pyclient/agave_pyclient/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Daniel Toloudis, Allen Institute for Cell Science"
 __email__ = "danielt@alleninstitute.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 
 def get_module_version() -> str:
