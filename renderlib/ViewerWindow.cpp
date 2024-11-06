44801b00c21f6f16f747f9561475203261e27606
dmt <dmt@completelydifferent.com>
2024-11-05 20:43:39 -0800

    start adding imgui rendering code

--- renderlib/ViewerWindow.cpp
+++ renderlib/ViewerWindow.cpp
@@ -13,6 +13,7 @@
 #include "renderlib.h"
 
 #include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
 
 ViewerWindow::ViewerWindow(RenderSettings* rs)
   : m_renderSettings(rs)
@@ -28,6 +29,44 @@ ViewerWindow::ViewerWindow(RenderSettings* rs)
   m_areaLightTool = new AreaLightTool();
   m_tools.push_back(new ScaleBarTool());
   m_tools.push_back(new AxisHelperTool());
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+  // GL ES 2.0 + GLSL 100
+  const char* glsl_version = "#version 100";
+#elif defined(__APPLE__)
+  // GL 3.2 + GLSL 150
+  const char* glsl_version = "#version 150";
+#else
+  // GL 3.0 + GLSL 130
+  const char* glsl_version = "#version 130";
+#endif
+
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void)io;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
+  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
+  // io.ConfigViewportsNoAutoMerge = true;
+  // io.ConfigViewportsNoTaskBarIcon = true;
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  // ImGui::StyleColorsLight();
+
+  // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+  ImGuiStyle& style = ImGui::GetStyle();
+  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+    style.WindowRounding = 0.0f;
+    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+  }
+
+  // Setup Platform/Renderer backends
+  // ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init(glsl_version);
 }
 
 ViewerWindow::~ViewerWindow()
@@ -201,7 +240,13 @@ ViewerWindow::redraw()
     m_increments = 0;
   }
 
+  // Start the Dear ImGui frame
+  ImGui_ImplOpenGL3_NewFrame();
+  // ImGui_ImplGlfw_NewFrame();
   ImGui::NewFrame();
+
+  const auto show_demo_window = true;
+  ImGui::ShowDemoWindow(&show_demo_window);
 
   glm::ivec2 oldpickbuffersize = m_selection.resolution;
   bool ok = m_selection.update(glm::ivec2(width(), height()));
