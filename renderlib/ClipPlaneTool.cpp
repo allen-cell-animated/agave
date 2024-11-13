ba3e7ac83bcd813caaa00ff714ca94eb5b8daf23
toloudis <danielt@alleninstitute.org>
2024-11-12 16:44:28 -0800

    position plane drawing consistently

--- renderlib/ClipPlaneTool.cpp
+++ renderlib/ClipPlaneTool.cpp
@@ -44,12 +44,13 @@ ClipPlaneTool::draw(SceneView& scene, Gesture& gesture)
                              Gesture::Graphics::VertsCode(p1, color, opacity, code));
   }
 
+  opacity = 0.3f;
   gesture.graphics.addCommand(Gesture::Graphics::PrimitiveType::kTriangles);
   // fill in the rectangle of the plane spanning the u and v axes
-  glm::vec3 p0 = p - u * scale - v * scale;
-  glm::vec3 p1 = p + u * scale - v * scale;
-  glm::vec3 p2 = p + u * scale + v * scale;
-  glm::vec3 p3 = p - u * scale + v * scale;
+  glm::vec3 p0 = p - u * (0.5f * scale) - v * (0.5f * scale);
+  glm::vec3 p1 = p + u * (0.5f * scale) - v * (0.5f * scale);
+  glm::vec3 p2 = p + u * (0.5f * scale) + v * (0.5f * scale);
+  glm::vec3 p3 = p - u * (0.5f * scale) + v * (0.5f * scale);
   gesture.graphics.addVert(Gesture::Graphics::VertsCode(p0, color, opacity, code));
   gesture.graphics.addVert(Gesture::Graphics::VertsCode(p1, color, opacity, code));
   gesture.graphics.addVert(Gesture::Graphics::VertsCode(p2, color, opacity, code));
