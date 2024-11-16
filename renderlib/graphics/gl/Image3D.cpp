ae6534cc9984edb229f4085c40acb12c5a3af5d9
toloudis <danielt@alleninstitute.org>
2024-11-15 17:25:28 -0800

    clip in raymarch mode too

--- renderlib/graphics/gl/Image3D.cpp
+++ renderlib/graphics/gl/Image3D.cpp
@@ -87,7 +87,6 @@ Image3D::render(const CCamera& camera, const Scene* scene, const RenderSettings*
   m_image3d_shader->resolution = glm::vec2(camera.m_Film.GetWidth(), camera.m_Film.GetHeight());
   m_image3d_shader->isPerspective = (camera.m_Projection == PERSPECTIVE) ? 1.0f : 0.0f;
   m_image3d_shader->orthoScale = camera.m_OrthoScale;
-  m_image3d_shader->setShadingUniforms();
 
   // move the box to match where the camera is pointed
   // transform the box from -0.5..0.5 to 0..physicalsize
@@ -101,6 +100,16 @@ Image3D::render(const CCamera& camera, const Scene* scene, const RenderSettings*
   glm::mat4 mm = glm::scale(glm::mat4(1.0f), scales);
   mm = glm::translate(mm, glm::vec3(0.5, 0.5, 0.5));
 
+  // TODO transform plane from world to object space. use inverse of mm
+  if (scene->m_clipPlane->m_enabled) {
+    Plane plane = scene->m_clipPlane->m_plane;
+    plane = plane.transform(glm::inverse(mm)); // * plane;
+    m_image3d_shader->clipPlane = plane.asVec4();
+  } else {
+    m_image3d_shader->clipPlane = glm::vec4(0, 0, 0, 0);
+  }
+
+  m_image3d_shader->setShadingUniforms();
   m_image3d_shader->setTransformUniforms(camera, mm);
 
   glActiveTexture(GL_TEXTURE0);
