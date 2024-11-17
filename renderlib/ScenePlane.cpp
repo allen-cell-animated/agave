fef752605dffb64840f7970bf8d263edb3fea001
dmt <dmt@completelydifferent.com>
2024-11-17 15:14:28 -0800

    more math tweaking

--- renderlib/ScenePlane.cpp
+++ renderlib/ScenePlane.cpp
@@ -6,7 +6,7 @@ ScenePlane::ScenePlane(glm::vec3 pos)
 {
   m_center = pos;
   m_transform.m_center = pos;
-  m_plane = Plane().transform(m_transform.getMatrix());
+  m_plane = Plane(); //().transform(m_transform.getMatrix());
   assert(m_plane.isInPlane(pos));
   m_enabled = true;
   m_tool = std::make_unique<ClipPlaneTool>(m_plane, pos);
@@ -15,11 +15,16 @@ ScenePlane::ScenePlane(glm::vec3 pos)
 void
 ScenePlane::updateTransform()
 {
-  Plane p0;
-  Plane p = p0.transform(m_transform.getMatrix());
-  // m_center = m_center + m_transform.m_center;
-  //  rotation only about current plane center... for now
-  // p = Plane(p.normal, m_center + m_transform.m_center);
+  Plane p;
+
+  // strategy: keep all transform info in m_transform;
+  // the plane stored here is just in default coords.
+
+  // rotate the plane normal according to the transform's rotation.
+  // then translate the plane center.
+  // then rebuild the plane using the new plane center.
+
+  m_center = m_transform.m_center;
 
   //  this lets the GUI have a chance to update in an abstract way
   for (auto it = m_observers.begin(); it != m_observers.end(); ++it) {
@@ -27,7 +32,7 @@ ScenePlane::updateTransform()
   }
 
   m_plane = p;
-  m_tool->m_plane = p;
+  // the clip plane tool should get the transformed plane and position to draw with
+  m_tool->m_plane = p.transform(m_transform.getMatrix());
   m_tool->m_pos = m_transform.m_center;
-  m_center = m_transform.m_center;
 }
