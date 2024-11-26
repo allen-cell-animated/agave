4cac7c517d087859889c8a39ce52082f65bf16c7
toloudis <danielt@alleninstitute.org>
2024-11-25 18:07:24 -0800

    commenting

--- renderlib/graphics/glsl/GLThickLines.cpp
+++ renderlib/graphics/glsl/GLThickLines.cpp
@@ -4,6 +4,95 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+static const char* vertex_shader_text =
+  R"(
+    #version 400 core
+
+    layout (location = 0) in vec3 vPos;
+    layout (location = 1) in vec2 vUV;
+    layout (location = 2) in vec4 vCol;
+    layout (location = 3) in uint vCode;
+
+    uniform mat4 projection;
+    uniform vec2 resolution;
+    uniform int picking;
+    uniform float thickness;
+
+    out vec4 Frag_color;
+    out vec2 Frag_UV;
+
+    void main()
+    {
+        Frag_UV = vUV;
+        if (picking == 1) {
+          Frag_color = vec4(float(vCode & 0xffu) / 255.0,
+                            float((vCode >> 8) & 0xffu) / 255.0,
+                            float((vCode >> 16) & 0xffu) / 255.0,
+                            1.0);
+        }
+        else {
+          Frag_color = vCol;
+        }
+
+
+
+    vec4 va[4];
+    // put everyhing in pixel space so we can apply thickness and 
+    // compute miters
+    for (int i=0; i<4; ++i)
+    {
+        va[i] = projection * vertex[line_i+i];
+        va[i].xyz /= va[i].w;
+        va[i].xy = (va[i].xy + 1.0) * 0.5 * u_resolution;
+    }
+
+    vec2 v_line  = normalize(va[2].xy - va[1].xy);
+    vec2 nv_line = vec2(-v_line.y, v_line.x);
+
+    vec4 pos;
+    if (tri_i == 0 || tri_i == 1 || tri_i == 3)
+    {
+        vec2 v_pred  = normalize(va[1].xy - va[0].xy);
+        vec2 v_miter = normalize(nv_line + vec2(-v_pred.y, v_pred.x));
+
+        pos = va[1];
+        pos.xy += v_miter * u_thickness * (tri_i == 1 ? -0.5 : 0.5) / dot(v_miter, nv_line);
+    }
+    else
+    {
+        vec2 v_succ  = normalize(va[3].xy - va[2].xy);
+        vec2 v_miter = normalize(nv_line + vec2(-v_succ.y, v_succ.x));
+
+        pos = va[2];
+        pos.xy += v_miter * u_thickness * (tri_i == 5 ? 0.5 : -0.5) / dot(v_miter, nv_line);
+    }
+
+    // undo the perspective divide and go back to clip space
+    pos.xy = pos.xy / u_resolution * 2.0 - 1.0;
+    pos.xyz *= pos.w;
+    gl_Position = pos;
+    }
+    )";
+
+// * create an array with the points of the line strip.
+// * first and last point define the tangents of the start and end of the line strip,
+// so you need to add one pt at start and end
+// * if drawing a line loop, then the last point has to be added to the array head,
+// and the first point added to the tail
+// * store the array of pts in a buffer that the shader can access by indexing (ideally SSBO?)
+// * shader doesn't need any vertex coordinates or attributes. It just needs to know the index of the line segment.
+// * to get the line segment index we just use the index of the vertex currently being processed (gl_VertexID)
+// * to draw a line strip with N points (N-1 segments), we need 6*(N-1) vertices
+// * each segment is 2 triangles. glDrawArrays(GL_TRIANGLES, 0, 6*(N-1)) will draw the line strip
+// line index = gl_VertexID / 6
+// tri index = gl_VertexID % 6
+// * Since we are drawing N-1 line segments, but the number of elements in the array is N+2,
+// the elements form vertex[line_t] to vertex[line_t+3] can be accessed for each vertex which
+// is processed in the vertex shader.
+// * vertex[line_t+1] and vertex[line_t+2] are the start and end coordinate of the line segment.
+// * vertex[line_t] and vertex[line_t+3] are required to compute the miter.
+// thickness is provided in pixels, and so we need to convert it to clip space and use window resolution
+
 std::string vertShader = R"(
 #version 460
 
