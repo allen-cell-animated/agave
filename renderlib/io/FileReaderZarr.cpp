6b94610825bb2e5c98edcd799dcd164be1cfb6d7
DMT <danielt@alleninstitute.org>
2024-12-28 19:05:17 -0800

    one more try explicit templating

--- renderlib/io/FileReaderZarr.cpp
+++ renderlib/io/FileReaderZarr.cpp
@@ -533,16 +533,20 @@ FileReaderZarr::loadFromFile(const LoadSpec& loadSpec)
 
     tensorstore::Index shapeToLoad[5] = { 1, 1, dims.sizeZ, dims.sizeY, dims.sizeX };
     if (levelDims.dtype == "uint8") {
-      tensorstore::Array<uint8_t, 5> arr((uint8_t*)reinterpret_cast<uint8_t*>(destptr), shapeToLoad);
+      tensorstore::Array<uint8_t, 5, tensorstore::zero_origin, tensorstore::container> arr(
+        (uint8_t*)reinterpret_cast<uint8_t*>(destptr), shapeToLoad, tensorstore::c_order);
       tensorstore::Read(m_store | transform, tensorstore::UnownedToShared(arr)).value();
     } else if (levelDims.dtype == "int32") {
-      tensorstore::Array<int32_t, 5> arr((int32_t*)reinterpret_cast<int32_t*>(destptr), shapeToLoad);
+      tensorstore::Array<int32_t, 5, tensorstore::zero_origin, tensorstore::container> arr(
+        (int32_t*)reinterpret_cast<int32_t*>(destptr), shapeToLoad, tensorstore::c_order);
       tensorstore::Read(m_store | transform, tensorstore::UnownedToShared(arr)).value();
     } else if (levelDims.dtype == "uint16") {
-      tensorstore::Array<uint16_t, 5> arr((uint16_t*)reinterpret_cast<uint16_t*>(destptr), shapeToLoad);
+      tensorstore::Array<uint16_t, 5, tensorstore::zero_origin, tensorstore::container> arr(
+        (uint16_t*)reinterpret_cast<uint16_t*>(destptr), shapeToLoad, tensorstore::c_order);
       tensorstore::Read(m_store | transform, tensorstore::UnownedToShared(arr)).value();
     } else if (levelDims.dtype == "float32") {
-      tensorstore::Array<float, 5> arr((float*)reinterpret_cast<float*>(destptr), shapeToLoad);
+      tensorstore::Array<float, 5, tensorstore::zero_origin, tensorstore::container> arr(
+        (float*)reinterpret_cast<float*>(destptr), shapeToLoad, tensorstore::c_order);
       tensorstore::Read(m_store | transform, tensorstore::UnownedToShared(arr)).value();
     } else {
       LOG_ERROR << "Unrecognized format (" << levelDims.dtype
