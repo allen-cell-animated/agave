429be343cd5b299f0be6b6a8f410bc7f12c4950a
dmt <dmt@completelydifferent.com>
2025-01-04 12:38:34 -0800

    cleaner impl

--- renderlib/AppScene.cpp
+++ renderlib/AppScene.cpp
@@ -75,12 +75,13 @@ Lighting::Lighting(const Lighting& other)
 {
   m_NoLights = other.m_NoLights;
   for (int i = 0; i < MAX_NO_LIGHTS; ++i) {
+    if (other.m_Lights[i]) {
+      m_Lights[i] = new Light(*other.m_Lights[i]);
+      m_sceneLights[i] = new SceneLight(m_Lights[i]);
+    } else {
       m_Lights[i] = nullptr;
       m_sceneLights[i] = nullptr;
-  }
-  for (int i = 0; i < m_NoLights; ++i) {
-    m_Lights[i] = new Light(*other.m_Lights[i]);
-    m_sceneLights[i] = new SceneLight(m_Lights[i]);
+    }
   }
 }
 
