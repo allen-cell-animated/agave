fdcc31363218b77e38c14ac35905deae67de17c9
Dan Toloudis <danielt@alleninstitute.org>
2025-02-13 15:42:06 -0800

    revert

--- renderlib/io/FileReaderZarr.cpp
+++ renderlib/io/FileReaderZarr.cpp
@@ -339,7 +339,8 @@ FileReaderZarr::loadMultiscaleDims(const std::string& filepath, uint32_t scene)
             LOG_ERROR << "Error: " << result.status();
             LOG_ERROR << "Failed to open store for " << filepath << " :: " << pathstr;
           } else {
-            tensorstore::TensorStore<> store = result.value();
+            auto store = result.value();
+
             tensorstore::DataType dtype = store.dtype();
             auto shape_span = store.domain().shape();
             std::cout << "Level " << multiscaleDims.size() << " shape " << shape_span << std::endl;
