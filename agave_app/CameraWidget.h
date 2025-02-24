633d44dabfa515726805f27bc7840df51fa7fb35
Dan Toloudis <danielt@alleninstitute.org>
2025-02-24 14:28:37 -0800

    start building out a better form builder for any object properties

--- agave_app/CameraWidget.h
+++ agave_app/CameraWidget.h
@@ -3,12 +3,86 @@
 #include "Camera.h"
 #include "Controls.h"
 
+#include "renderlib/core/prty/prtyProperty.h"
+#include "renderlib/Logging.h"
+
 #include <QCheckBox>
 #include <QComboBox>
 #include <QFormLayout>
 #include <QWidget>
 
 class RenderSettings;
+
+class CameraDataObject
+{
+public:
+  CameraDataObject()
+    : m_camera(nullptr)
+  {
+    // hook up properties to update the underlying camera
+    Exposure.addCallback([this](prtyProperty<float>* p, bool) {
+      LOG_DEBUG << "Setting exposure to " << p->get();
+      // m_camera->GetFilm().SetExposure(val);
+    });
+    ExposureIterations.addCallback([this](prtyProperty<int>* p, bool) {
+      LOG_DEBUG << "Setting exposure iterations to " << p->get();
+      // m_camera->GetFilm().SetExposureIterations(val);
+    });
+    NoiseReduction.addCallback([this](prtyProperty<bool>* p, bool) {
+      LOG_DEBUG << "Setting noise reduction to " << p->get();
+      // m_camera->GetFilm().SetNoiseReduction(val);
+    });
+    ApertureSize.addCallback([this](prtyProperty<float>* p, bool) {
+      LOG_DEBUG << "Setting aperture size to " << p->get();
+      // m_camera->GetAperture().SetSize(val);
+    });
+    FieldOfView.addCallback([this](prtyProperty<float>* p, bool) {
+      LOG_DEBUG << "Setting field of view to " << p->get();
+      // m_camera->GetProjection().SetFieldOfView(val);
+    });
+    FocalDistance.addCallback([this](prtyProperty<float>* p, bool) {
+      LOG_DEBUG << "Setting focal distance to " << p->get();
+      // m_camera->GetFocus().SetFocalDistance(val);
+    });
+  }
+  CameraDataObject(CCamera* camera)
+    : m_camera(camera)
+  {
+    // hook up properties to update the underlying camera
+    Exposure.addCallback([this](prtyProperty<float>* p, bool) {
+      LOG_DEBUG << "Setting exposure to " << p->get();
+      // m_camera->GetFilm().SetExposure(val);
+    });
+    ExposureIterations.addCallback([this](prtyProperty<int>* p, bool) {
+      LOG_DEBUG << "Setting exposure iterations to " << p->get();
+      // m_camera->GetFilm().SetExposureIterations(val);
+    });
+    NoiseReduction.addCallback([this](prtyProperty<bool>* p, bool) {
+      LOG_DEBUG << "Setting noise reduction to " << p->get();
+      // m_camera->GetFilm().SetNoiseReduction(val);
+    });
+    ApertureSize.addCallback([this](prtyProperty<float>* p, bool) {
+      LOG_DEBUG << "Setting aperture size to " << p->get();
+      // m_camera->GetAperture().SetSize(val);
+    });
+    FieldOfView.addCallback([this](prtyProperty<float>* p, bool) {
+      LOG_DEBUG << "Setting field of view to " << p->get();
+      // m_camera->GetProjection().SetFieldOfView(val);
+    });
+    FocalDistance.addCallback([this](prtyProperty<float>* p, bool) {
+      LOG_DEBUG << "Setting focal distance to " << p->get();
+      // m_camera->GetFocus().SetFocalDistance(val);
+    });
+  }
+  prtyProperty<float> Exposure{ "Exposure", 0.75f };
+  prtyProperty<int> ExposureIterations{ "ExposureIterations", 1 };
+  prtyProperty<bool> NoiseReduction{ "NoiseReduction", false };
+  prtyProperty<float> ApertureSize{ "ApertureSize", 0.0f };
+  prtyProperty<float> FieldOfView{ "FieldOfView", 30.0f };
+  prtyProperty<float> FocalDistance{ "FocalDistance", 0.0f };
+
+  CCamera* m_camera;
+};
 
 class QCameraWidget : public QWidget
 {
@@ -44,4 +118,7 @@ private slots:
   void OnApertureChanged();
   void OnFocusChanged();
   void OnProjectionChanged();
+
+private:
+  CameraDataObject m_cameraProps;
 };
--- renderlib/core/prty/prtyProperty.h
+++ renderlib/core/prty/prtyProperty.h
@@ -1,38 +1,73 @@
 #pragma once
 
+#include <functional>
+#include <memory>
 #include <string>
 #include <type_traits>
 
 // a property has a name and a value.
 // the init value is default-constructed or passed in.
 // The value type must be copyable, assignable, and default-constructible.
-template <typename T, typename = std::enable_if_t<
-std::is_copy_constructible_v<T> &&
-std::is_copy_assignable_v<T> &&
-std::is_default_constructible_v<T>>>
-class prtyProperty {
+template<typename T,
+         typename = std::enable_if_t<std::is_copy_constructible_v<T> && std::is_copy_assignable_v<T> &&
+                                     std::is_default_constructible_v<T> && std::is_move_constructible_v<T> &&
+                                     std::is_move_assignable_v<T>>>
+class prtyProperty
+{
 public:
-    prtyProperty(std::string name) : name(name) {}
-    prtyProperty(std::string name, const T& val) : name(name), value(val) {}
-    virtual ~prtyProperty() {}
-  void set(const T& val) {value=val; }
+  prtyProperty(std::string name)
+    : name(name)
+  {
+  }
+
+  prtyProperty(std::string name, const T& val)
+    : name(name)
+    , value(val)
+  {
+  }
+
+  virtual ~prtyProperty() {}
+
+  void set(const T& val, bool isFromUI = false)
+  {
+    // TODO - check if the value has changed
+
+    value = val;
+
+    // call all callbacks
+    notifyAll(isFromUI);
+  }
+
   // copy????
   T get() const { return value; }
-  // non-copy, just use paren operator
-  T& operator() const { return value; }
 
+  // non-copy, just use paren operator
+  T& operator()() const { return value; }
 
   // set up the rule of 5
-  prtyProperty(const prtyProperty& other) : name(other.name), value(other.value) {}
-  prtyProperty& operator=(const prtyProperty& other) {
+  prtyProperty(const prtyProperty& other)
+    : name(other.name)
+    , value(other.value)
+  {
+  }
+
+  prtyProperty& operator=(const prtyProperty& other)
+  {
     if (this != &other) {
       name = other.name;
       value = other.value;
     }
     return *this;
   }
-  prtyProperty(prtyProperty&& other) : name(std::move(other.name)), value(std::move(other.value)) {}
-  prtyProperty& operator=(prtyProperty&& other) {
+
+  prtyProperty(prtyProperty&& other)
+    : name(std::move(other.name))
+    , value(std::move(other.value))
+  {
+  }
+
+  prtyProperty& operator=(prtyProperty&& other)
+  {
     if (this != &other) {
       name = std::move(other.name);
       value = std::move(other.value);
@@ -41,12 +76,37 @@ public:
   }
 
   // direct assignment from a value
-  prtyProperty& operator=(const T& val) {
+  prtyProperty& operator=(const T& val)
+  {
     value = val;
     return *this;
+  }
+
+  // callback for when the property's value is set.
+  // The boolean flag is true
+  //	if the change is coming from the user interface and therefore
+  //	should mark the document containing the property as dirty.
+  typedef std::function<void(prtyProperty<T>*, bool)> prtyPropertyCallback;
+
+  void addCallback(prtyPropertyCallback cb) { callbacks.push_back(cb); }
+  void removeCallback(prtyPropertyCallback cb)
+  {
+    auto it = std::find(callbacks.begin(), callbacks.end(), cb);
+    if (it != callbacks.end()) {
+      callbacks.erase(it);
+    }
+  }
+
+  void notifyAll(bool isFromUI = false)
+  {
+    for (auto& cb : callbacks) {
+      cb(this, isFromUI);
+    }
   }
 
 private:
   std::string name;
   T value;
+
+  std::vector<prtyPropertyCallback> callbacks;
 };
