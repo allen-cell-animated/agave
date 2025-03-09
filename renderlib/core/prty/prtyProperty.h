0fbcf6a3c5b7a8ee88e179759dbac693d1da1437
dmt <dmt@completelydifferent.com>
2025-03-09 09:59:40 -0700

    use a trait for streamable property value types for debugging

--- renderlib/core/prty/prtyProperty.h
+++ renderlib/core/prty/prtyProperty.h
@@ -1,17 +1,32 @@
 #pragma once
 
+#include "Logging.h"
+
 #include <functional>
+#include <iostream>
 #include <memory>
 #include <string>
+#include <sstream>
 #include <type_traits>
+
+// only enable this to verbosely dump every single property setter.
+// This implies that the value type must be streamable to std::ostream.
+#ifndef PRTY_PROPERTY_DEBUG
+#define PRTY_PROPERTY_DEBUG 0
+#endif
 
 // a property has a name and a value.
 // the init value is default-constructed or passed in.
 // The value type must be copyable, assignable, and default-constructible.
+// For debugging, the value must also be streamable to std::ostream.
 template<typename T,
-         typename = std::enable_if_t<std::is_copy_constructible_v<T> && std::is_copy_assignable_v<T> &&
-                                     std::is_default_constructible_v<T> && std::is_move_constructible_v<T> &&
-                                     std::is_move_assignable_v<T>>>
+         typename = std::enable_if_t<
+           std::is_copy_constructible_v<T> && std::is_copy_assignable_v<T> && std::is_default_constructible_v<T> &&
+           std::is_move_constructible_v<T> && std::is_move_assignable_v<T>
+#if PRTY_PROPERTY_DEBUG
+           && std::is_convertible_v<decltype(std::declval<std::ostream&>() << std::declval<T>()), std::ostream&>
+#endif
+           >>
 class prtyProperty
 {
 public:
@@ -32,6 +47,11 @@ public:
   void set(const T& val, bool isFromUI = false)
   {
     // TODO - check if the value has changed
+    // TODO - do we notify if value hasn't changed?
+
+#if PRTY_PROPERTY_DEBUG
+    LOG_INFO << "Property " << name << " set from " << value << " to " << val;
+#endif
 
     value = val;
 
