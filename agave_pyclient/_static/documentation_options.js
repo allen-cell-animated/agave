9a613027f122c42d74d6cad1f3555ce4a3f48442
toloudis <toloudis@users.noreply.github.com>
2025-05-17 03:39:34 +0000

    Deploying to gh-pages from @ allen-cell-animated/agave@109c0571d5fa8b1a007de041204d4c8c3639aeea 🚀

--- agave_pyclient/_static/documentation_options.js
+++ agave_pyclient/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.0-rc.0',
+    VERSION: '1.8.0-rc.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
