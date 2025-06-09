9922f8863a15c109f91dcaa4b1038d43908f75d1
dmt <dmt@completelydifferent.com>
2025-06-09 07:32:05 -0700

    try to hook up changes that need to happen on the ViewerWindow

--- agave_app/AppearanceDockWidget2.cpp
+++ agave_app/AppearanceDockWidget2.cpp
@@ -1,8 +1,11 @@
 #include "AppearanceDockWidget2.h"
 
-QAppearanceDockWidget2::QAppearanceDockWidget2(QWidget* pParent, RenderSettings* rs, AppearanceDataObject* ado)
+QAppearanceDockWidget2::QAppearanceDockWidget2(QWidget* pParent,
+                                               RenderSettings* rs,
+                                               ViewerWindow* vw,
+                                               AppearanceDataObject* ado)
   : QDockWidget(pParent)
-  , m_AppearanceWidget(nullptr, rs, ado)
+  , m_AppearanceWidget(nullptr, rs, vw, ado)
 {
   setWindowTitle("Appearance");
 
