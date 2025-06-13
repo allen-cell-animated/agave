d12cfdbc47d428243a6609727b4ddfe0f08d4f0c
Daniel Toloudis <danielt@alleninstitute.org>
2025-06-13 09:25:18 -0700

    Bump to 1.8.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Allen Institute for Cell Science"
 author = "Allen Institute for Cell Science"
 
 # The full version, including alpha/beta/rc tags
-release = "1.8.0"
+release = "1.8.1"
 
 
 # -- General configuration ---------------------------------------------------
