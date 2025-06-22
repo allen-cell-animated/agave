2038a7f99aeb11206dfa4d02143b0c0ce31903c9
dmt <dmt@completelydifferent.com>
2025-06-21 22:35:20 -0700

    fix test, add callbacks

--- test/test_prty.cpp
+++ test/test_prty.cpp
@@ -20,13 +20,11 @@ TEST_CASE("prtyProperty", "[prtyProperty]")
   REQUIRE(p.GetValue() == 42);
   REQUIRE(p == 42);
   REQUIRE(p.GetPropertyName() == "int");
-  REQUIRE(p.GetType() == "int8_t");
   REQUIRE(p != 43);
 
   prtyText p2("string", "hello");
   REQUIRE(p2.GetValue() == "hello");
   REQUIRE(p2.GetPropertyName() == "string");
-  REQUIRE(p2.GetType() == "std::string");
   REQUIRE(p2 == "hello");
   REQUIRE(p2 != "world");
   p2.SetValue("world");
