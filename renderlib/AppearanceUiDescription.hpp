061bc665809b8b30dbd7f478fa5e0a3160b20af1
dmt <dmt@completelydifferent.com>
2025-06-21 17:30:07 -0700

    WIP

--- renderlib/AppearanceUiDescription.hpp
+++ renderlib/AppearanceUiDescription.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "AppearanceDataObject.hpp"
+#include "core/prty/prtyObject.hpp"
+#include "RenderSettings.h"
 #include "uiInfo.hpp"
 
 struct AppearanceUiDescription
@@ -15,4 +18,33 @@ struct AppearanceUiDescription
   static CheckBoxUiInfo m_showBoundingBox;
   static ColorPickerUiInfo m_boundingBoxColor;
   static CheckBoxUiInfo m_showScaleBar;
+};
+
+class AppearanceObject : public prtyObject
+{
+public:
+  AppearanceObject();
+
+  void updatePropsFromObject();
+  void updateObjectFromProps();
+
+private:
+  // the properties
+  AppearanceDataObject m_appearanceDataObject;
+
+  // the actual camera
+  std::shared_ptr<RenderSettings> m_renderSettings;
+
+  // the ui info
+  ComboBoxUiInfo* m_rendererType;
+  ComboBoxUiInfo* m_shadingType;
+  FloatSliderSpinnerUiInfo* m_densityScale;
+  FloatSliderSpinnerUiInfo* m_gradientFactor;
+  FloatSliderSpinnerUiInfo* m_stepSizePrimaryRay;
+  FloatSliderSpinnerUiInfo* m_stepSizeSecondaryRay;
+  CheckBoxUiInfo* m_interpolate;
+  ColorPickerUiInfo* m_backgroundColor;
+  CheckBoxUiInfo* m_showBoundingBox;
+  ColorPickerUiInfo* m_boundingBoxColor;
+  CheckBoxUiInfo* m_showScaleBar;
 };
---
+++ renderlib/CameraUiDescription.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CameraDataObject.hpp"
+#include "uiInfo.hpp"
+#include "core/prty/prtyObject.hpp"
+#include "CCamera.h"
+
+struct CameraUiDescription
+{
+  static FloatSliderSpinnerUiInfo m_exposure;
+  static ComboBoxUiInfo m_exposureIterations;
+  static CheckBoxUiInfo m_noiseReduction;
+  static FloatSliderSpinnerUiInfo m_apertureSize;
+  static FloatSliderSpinnerUiInfo m_fieldOfView;
+  static FloatSliderSpinnerUiInfo m_focalDistance;
+};
+
+class CameraObject : public prtyObject
+{
+public:
+  CameraObject();
+
+  void updatePropsFromObject();
+  void updateObjectFromProps();
+
+private:
+  // the properties
+  CameraDataObject m_cameraDataObject;
+
+  // the actual camera
+  std::shared_ptr<CCamera> m_camera;
+
+  // the ui info
+  FloatSliderSpinnerUiInfo* m_ExposureUIInfo;
+  ComboBoxUiInfo* m_ExposureIterationsUIInfo;
+  CheckBoxUiInfo* m_NoiseReductionUIInfo;
+  FloatSliderSpinnerUiInfo* m_ApertureSizeUIInfo;
+  FloatSliderSpinnerUiInfo* m_FieldOfViewUIInfo;
+  FloatSliderSpinnerUiInfo* m_FocalDistanceUIInfo;
+};
