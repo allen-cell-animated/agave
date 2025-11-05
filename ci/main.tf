fd180f3479abf8ec89413f6beaf2fd8f603b7318
Daniel Toloudis <danielt@alleninstitute.org>
2025-11-04 18:16:36 -0800

    add two new dockerfiles

--- ci/main.tf
+++ ci/main.tf
@@ -250,7 +250,7 @@ resource "aws_ecs_service" "agave_service" {
 # Note: If you receive an error using the AWS CLI, make sure that you have the latest version of the AWS CLI and Docker installed.
 # 2. Build your Docker image using the following command. For information on building a Docker file from scratch see the instructions here . You can skip this step if your image is already built:
 #
-# docker build -t agave-ecr-repo .
+# docker build -t agave-ecr-repo -f docker/Dockerfile .
 
 # 3. After the build completes, tag your image so you can push the image to this repository:
 #
