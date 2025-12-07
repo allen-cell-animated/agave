5700931bfca6faf24cf61eaac70cf4cc7fd191b9
dmt <dmt@completelydifferent.com>
2025-12-06 16:38:14 -0800

    fix AppearanceObject.hpp

--- renderlib/AppearanceObject.hpp
+++ renderlib/AppearanceObject.hpp
@@ -3,6 +3,7 @@
 #include "AppearanceDataObject.hpp"
 #include "core/prty/prtyObject.hpp"
 #include "RenderSettings.h"
+#include "AppScene.h"
 #include "uiInfo.hpp"
 
 struct AppearanceUiDescription
@@ -29,7 +30,7 @@ public:
   void updateObjectFromProps();
 
   // Getter for appearance data object
-  // AppearanceDataObject& getAppearanceDataObject() { return m_appearanceDataObject; }
+  AppearanceDataObject& appearanceDataObject() { return m_appearanceDataObject; }
   const AppearanceDataObject& getAppearanceDataObject() const { return m_appearanceDataObject; }
 
   // Getters for UI info objects
@@ -52,8 +53,9 @@ private:
   // the properties
   AppearanceDataObject m_appearanceDataObject;
 
-  // the actual camera
+  // the actual settings
   std::shared_ptr<RenderSettings> m_renderSettings;
+  std::weak_ptr<Scene> m_scene;
 
   // the ui info
   ComboBoxUiInfo* m_rendererType;
@@ -67,4 +69,16 @@ private:
   CheckBoxUiInfo* m_showBoundingBox;
   ColorPickerUiInfo* m_boundingBoxColor;
   CheckBoxUiInfo* m_showScaleBar;
+
+  void RendererTypeChanged(prtyProperty* i_Property, bool i_bDirty);
+  void ShadingTypeChanged(prtyProperty* i_Property, bool i_bDirty);
+  void DensityScaleChanged(prtyProperty* i_Property, bool i_bDirty);
+  void GradientFactorChanged(prtyProperty* i_Property, bool i_bDirty);
+  void StepSizePrimaryRayChanged(prtyProperty* i_Property, bool i_bDirty);
+  void StepSizeSecondaryRayChanged(prtyProperty* i_Property, bool i_bDirty);
+  void InterpolateChanged(prtyProperty* i_Property, bool i_bDirty);
+  void BackgroundColorChanged(prtyProperty* i_Property, bool i_bDirty);
+  void ShowBoundingBoxChanged(prtyProperty* i_Property, bool i_bDirty);
+  void BoundingBoxColorChanged(prtyProperty* i_Property, bool i_bDirty);
+  void ShowScaleBarChanged(prtyProperty* i_Property, bool i_bDirty);
 };
