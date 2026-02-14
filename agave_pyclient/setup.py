436d49376f4403fad1d1e623d8e4b098bc47ae09
toloudis <toloudis@users.noreply.github.com>
2026-02-14 09:27:11 -0800

    Bump to 1.9.0-rc.4

--- agave_pyclient/setup.py
+++ agave_pyclient/setup.py
@@ -89,6 +89,6 @@ setup(
     url="https://github.com/allen-cell-animated/agave",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.9.0-rc.3",
+    version="1.9.0-rc.4",
     zip_safe=False,
 )
